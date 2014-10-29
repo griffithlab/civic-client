7b83bd7eacdb42f73c13124f6b4aa3f35727037c
Joshua McMichael <jmcmicha@genome.wustl.edu>
2014-10-29 11:04:41 -0500

    gene links on events page now show gene summary by default

--- src/app/views/events/genes/GenesViewCtrl.js
+++ src/app/views/events/genes/GenesViewCtrl.js
@@ -4,15 +4,18 @@
     .controller('GenesViewCtrl', GenesViewCtrl);
 
   // @ngInject
-  function GenesViewCtrl($log, $rootScope, $scope, $stateParams, Genes) {
+  function GenesViewCtrl($log, $rootScope, $scope, $state,  $stateParams, Genes) {
     $log.info("GenesViewCtrl loaded.");
 
-    var geneId = $stateParams.geneId;
-
-    $rootScope.setNavMode('sub');
-    $rootScope.setTitle('Event ' + geneId + ' / ...')
-
-    $scope.gene = Genes.get({'geneId': geneId });
+    $scope.gene = {};
+    // if no geneId supplied, reroute to /events so that user can choose a gene
+    if($stateParams.geneId) {
+      $scope.gene = Genes.get({'geneId': $stateParams.geneId });
+      $rootScope.setNavMode('sub');
+      $rootScope.setTitle('Event ' + $stateParams.geneId + ' / ...')
+    } else {
+      $state.go('events');
+    }
 
   }
 })();
