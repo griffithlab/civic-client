5ae1361aa3e73a562455fb026357030cd7183a15
Joshua McMichael <jmcmicha@genome.wustl.edu>
2014-11-03 13:21:54 -0600

    implemented rowClick on evidenceTable rows

--- src/app/views/events/variants/directives/evidenceTable.js
+++ src/app/views/events/variants/directives/evidenceTable.js
@@ -10,8 +10,14 @@
       templateUrl: '/civic-client/views/events/variants/directives/evidenceTable.tpl.html',
       replace: true,
       scope: false,
-      link: function($scope) {
-        console.log('evidenceTable linked.');
+      controller: function($scope, $state, $stateParams, $location, $log) {
+        $scope.rowClick = function(evidenceId) {
+          $state.go('events.genes.summary.variants.summary.evidence.summary', {
+            geneId: $stateParams.geneId,
+            variantId: $stateParams.variantId,
+            evidenceId: evidenceId
+          })
+        };
       }
     };
 
