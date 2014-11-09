b9fa41289e1c87b7bc14c28c7276cd5dd227ec58
gkarthik <xraydiffraction@gmail.com>
2014-11-08 18:10:28 -0800

    Added pathway source labels. Included all pathways sources.

--- src/app/common/services/GenesService.js
+++ src/app/common/services/GenesService.js
@@ -23,11 +23,40 @@
           method: 'JSONP',
           isArray: false,
           transformResponse: function(data){
-            if(data.pathway){  
-              if(!angular.isArray(data.pathway.pharmgkb) && data.pathway.pharmgkb){
-                data.pathway.pharmgkb = [data.pathway.pharmgkb];
+            var src_map = {
+              kegg: "http://www.genome.jp/kegg-bin/show_pathway?",
+              reactome: "http://www.reactome.org/cgi-bin/control_panel_st_id?ST_ID=",
+              pharmgkb: "https://www.pharmgkb.org/pathway/",
+              humancyc: "http://humancyc.org/HUMAN/NEW-IMAGE?type=PATHWAY&object=",
+              smpdb: "http://www.smpdb.ca/view/",
+              pid: "http://pid.nci.nih.gov/search/pathway_landing.shtml?what=graphic&jpg=on&pathway_id=",
+              wikipathways: "http://wikipathways.org/index.php/Pathway:",
+              netpath: null,
+              biocarta: null,
+              inoh: null,
+              signalink: null,
+              ehmn: null              
+            };
+            var pathways = data.pathway || [];
+            var pathways_final = [];
+            var link;
+            for(var src in pathways){
+              if(!angular.isArray(pathways[src])){
+                pathways[src] = [pathways[src]];
               }
-            }
+                for(var p in pathways[src]){
+                  link = src_map[src]+pathways[src][p].id;
+                  if(src_map[src] == null){
+                    link = null;
+                  }
+                  pathways_final.push({
+                    name: pathways[src][p].name,
+                    link: link,
+                    src: src
+                  });
+                }
+              }
+            data.pathway = pathways_final;
             if(!angular.isArray(data.alias) && data.alias){
               data.alias = [data.alias];
             }
@@ -37,6 +66,7 @@
             return data;
           }
         }
+        
       });
 
     return Genes;
