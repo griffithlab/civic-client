c64baab9b6439372cd18a8f95cb91e7adbb72454
Joshua McMichael <jmcmicha@genome.wustl.edu>
2014-11-17 12:58:48 -0600

    basic ui-grid working in browse view

--- src/app/views/browse/BrowseCtrl.js
+++ src/app/views/browse/BrowseCtrl.js
@@ -11,40 +11,46 @@
     $rootScope.setNavMode('sub');
     $rootScope.setTitle('Browse Events');
 
-    $scope.tableParams = new ngTableParams({
-      page: 1,            // show first page
-      count: 25,          // count per page
-      sorting: {
-        entrez_gene: 'asc'     // initial sorting
-      }
-    }, {
-      total: 0,           // length of data
-      debugMode: true,
-      getData: function($defer, params) {
-        // ajax request to api
-        Browse.get(params.url(), function(data) {
-          // update table params
-          params.total(data.total);
+    $scope.events = {};
 
-          // format categories & protein function
-          _.each(data.result, function(event) {
-            event.gene_category = event.gene_category.join();
-            event.protein_function = event.protein_function.join();
-          });
-
-          // set new data
-          $defer.resolve(data.result);
-        });
-      }
+    Browse.get({}, function(data) {
+      $scope.events = data;
     });
 
-    $scope.rowClick = function(gene) {
-      $state.go('events.genes.summary.variants.summary', { geneId: gene.entrez_gene, variantId: gene.variant });
-//      $log.info('Clicked gene ' + ['/gene/', gene.entrez_gene, '/variant/', gene.variant].join(""));
-//      var loc = ['/events/genes/', gene.entrez_gene, '/variants/', gene.variant].join("");
-//      $log.info('location.path(' + loc + ')');
-//      $location.path(loc);
-    };
+//    $scope.tableParams = new ngTableParams({
+//      page: 1,            // show first page
+//      count: 25,          // count per page
+//      sorting: {
+//        entrez_gene: 'asc'     // initial sorting
+//      }
+//    }, {
+//      total: 0,           // length of data
+//      debugMode: true,
+//      getData: function($defer, params) {
+//        // ajax request to api
+//        Browse.get(params.url(), function(data) {
+//          // update table params
+//          params.total(data.total);
+//
+//          // format categories & protein function
+//          _.each(data.result, function(event) {
+//            event.gene_category = event.gene_category.join();
+//            event.protein_function = event.protein_function.join();
+//          });
+//
+//          // set new data
+//          $defer.resolve(data.result);
+//        });
+//      }
+//    });
+//
+//    $scope.rowClick = function(gene) {
+//      $state.go('events.genes.summary.variants.summary', { geneId: gene.entrez_gene, variantId: gene.variant });
+////      $log.info('Clicked gene ' + ['/gene/', gene.entrez_gene, '/variant/', gene.variant].join(""));
+////      var loc = ['/events/genes/', gene.entrez_gene, '/variants/', gene.variant].join("");
+////      $log.info('location.path(' + loc + ')');
+////      $location.path(loc);
+//    };
   }
 
 // @ngInject
