ac18d39ad846963455816a872738356299422e56
Joshua McMichael <jmcmicha@genome.wustl.edu>
2014-11-18 12:30:55 -0600

    browse grid row click now changes state

--- src/app/views/browse/BrowseCtrl.js
+++ src/app/views/browse/BrowseCtrl.js
@@ -5,7 +5,7 @@
     .filter('ceil', ceilFilter);
 
 // @ngInject
-  function BrowseCtrl($scope, $rootScope, uiGridConstants, Browse, $location, _, $log) {
+  function BrowseCtrl($scope, $rootScope, uiGridConstants, Browse, $state, _, $log) {
     $log.info('BrowseCtrl loaded');
     $rootScope.setNavMode('sub');
     $rootScope.setTitle('Browse Events');
@@ -16,7 +16,7 @@
       enableFiltering: true,
       enableColumnMenus: false,
       enableSorting: true,
-      rowTemplate: "<div ng-repeat=\"(colRenderIndex, col) in colContainer.renderedColumns track by col.colDef.name\" ng-click=\"getExternalScopes().rowClick()\" class=\"ui-grid-cell\" ng-class=\"{ 'ui-grid-row-header-cell': col.isRowHeader }\" ui-grid-cell></div>",
+      rowTemplate: '/civic-client/views/browse/browseGridRow.tpl.html',
       columnDefs: [
         { name: 'entrez_gene',
           enableFiltering: true,
@@ -34,8 +34,12 @@
     };
 
     $scope.gridInteractions = {
-      rowClick: function () {
-        $log.info('row clicked.');
+      rowClick: function (row) {
+        $log.info('row ' + row.uid + ' clicked.');
+        $state.go('events.genes.summary.variants.summary', { geneId: row.entity.entrez_id, variantId: row.entity.variant });
+      },
+      rowHover: function() {
+        $log.info('row hover');
       }
     };
 
@@ -44,43 +48,7 @@
       $scope.browseGridOptions.data = data.result;
 
     });
-
-//    $scope.tableParams = new ngTableParams({
-//      page: 1,            // show first page
-//      count: 25,          // count per page
-//      sorting: {
-//        entrez_gene: 'asc'     // initial sorting
-//      }
-//    }, {
-//      total: 0,           // length of data
-//      debugMode: true,
-//      getData: function($defer, params) {
-//        // ajax request to api
-//        Browse.get(params.url(), function(data) {
-//          // update table params
-//          params.total(data.total);
-//
-//          // format categories & protein function
-//          _.each(data.result, function(event) {
-//            event.gene_category = event.gene_category.join();
-//            event.protein_function = event.protein_function.join();
-//          });
-//
-//          // set new data
-//          $defer.resolve(data.result);
-//        });
-//      }
-//    });
-//
-//    $scope.rowClick = function(gene) {
-//      $state.go('events.genes.summary.variants.summary', { geneId: gene.entrez_gene, variantId: gene.variant });
-////      $log.info('Clicked gene ' + ['/gene/', gene.entrez_gene, '/variant/', gene.variant].join(""));
-////      var loc = ['/events/genes/', gene.entrez_gene, '/variants/', gene.variant].join("");
-////      $log.info('location.path(' + loc + ')');
-////      $location.path(loc);
-//    };
   }
-
 
   function ceilFilter() {
     return function(num) {
