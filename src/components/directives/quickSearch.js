5d46462ffd948180f33538eacf0093903ca1f3fb
Joshua McMichael <jmcmicha@genome.wustl.edu>
2015-03-03 16:36:56 -0600

    quicksearch now includes gene aliases

--- src/components/directives/quickSearch.js
+++ src/components/directives/quickSearch.js
@@ -5,19 +5,29 @@
     .controller('quickSearchCtrl', quickSearchCtrl);
 
   // @ngInject
-  function quickSearchCtrl($scope, $log, TypeAheadResults, $state, _) {
+  function quickSearchCtrl($scope, TypeAheadResults, $state, _) {
     $scope.getVariants = function (val) {
       return TypeAheadResults.query({ query: val }).$promise
         .then(function (response) {
           var labelLimit = 75;
           return _.map(response.result, function (event) {
             var label = event.entrez_gene + ' / ' + event.variant;
+
+            if (_.includes(event.terms, 'gene_aliases')) {
+              var aliasLabel = event.gene_aliases.length > 1 ? 'Aliases' : 'Alias';
+              label = label + ' -- ' + aliasLabel + ': ' + event.gene_aliases.join(', ');
+            }
+
             if (_.includes(event.terms, 'drug_names')) {
-              label = label + '; Drug(s): ' + event.drug_names.join(' ,');
+              var drugLabel = event.drug_names.length > 1 ? 'Drugs' : 'Drug';
+              label = label + ' -- ' + drugLabel + ': ' + event.drug_names.join(', ');
             }
+
             if (_.includes(event.terms, 'disease_names')) {
-              label = label + '; Disease(s): ' + event.disease_names.join(' ,');
+              var diseaseLabel = event.disease_names.length > 1 ? 'Diseases' : 'Disease';
+              label = label + ' -- ' + diseaseLabel + ': ' + event.disease_names.join(', ');
             }
+
             if (label.length > labelLimit) { label = _.trunc(label, labelLimit); }
 
             return {
@@ -31,7 +41,6 @@
         });
     };
 
-    /*jshint camelcase: false */
     $scope.onSelect = function($item, $model) {
       $state.go('events.genes.summary.variants.summary', {geneId: $item.gene, variantId: $item.variant_id});
       $scope.asyncSelected.model = ''; // clear typeahead
