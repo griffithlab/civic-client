2d828c625552eaad66ee16bf51519430c073bc65
Joshua McMichael <josh@jmcmichael.com>
2015-04-13 09:41:41 -0500

    working on testing myGeneInfo dialog popup

--- src/app/views/events/genes/summary/myGeneInfo.js
+++ src/app/views/events/genes/summary/myGeneInfo.js
@@ -2,6 +2,7 @@
   'use strict';
   angular.module('civic.events.genes')
     .controller('MyGeneInfoController', MyGeneInfoController)
+    .controller('MyGeneInfoDialogCtrl', MyGeneInfoDialogCtrl)
     .directive('myGeneInfo', function() {
       return {
         restrict: 'E',
@@ -14,13 +15,96 @@
     });
 
   // @ngInject
-  function MyGeneInfoController($scope) {
+  function MyGeneInfoController($scope, dialogs) {
     var ctrl = $scope.ctrl = {};
     ctrl.geneInfo = $scope.geneInfo;
 
     ctrl.viewGeneDetails = function() {
+      $scope.dlg = dialogs.create(
+        'app/views/events/genes/summary/myGeneInfoDialog.tpl.html',
+        'MyGeneInfoDialogCtrl',
+        $scope.geneDetails,
+        'lg'
+      );
+    };
+  }
 
+  // @ngInject
+  function MyGeneInfoDialogCtrl($scope, uiGridConstants, $modalInstance, data) {
+    $scope.proteinDomainsGridOptions = {
+      enableFiltering: true,
+      enableColumnMenus: false,
+      enableSorting: true,
+      minRowsToShow: 7,
+      columnDefs: [
+        { name: 'desc',
+          displayName: 'Protein Domains',
+          enableFiltering: true,
+          filter: {
+            condition: uiGridConstants.filter.CONTAINS
+          },
+          width: '50%'
+        },
+        { name: 'id',
+          displayName: 'ID',
+          enableFiltering: true,
+          filter: {
+            condition: uiGridConstants.filter.CONTAINS
+          },
+          width: '15%'
+        },
+        { name: 'short_desc',
+          displayName: 'Identifier',
+          filter: {
+            condition: uiGridConstants.filter.CONTAINS
+          },
+          enableFiltering: true
+        }
+      ]
     };
 
+    $scope.proteinDomainsGridOptions.data = data.interpro;
+
+    $scope.pathwaysGridOptions= {
+      enableFiltering: true,
+      enableColumnMenus: false,
+      enableSorting: true,
+      minRowsToShow: 7,
+      filter: {
+        condition: uiGridConstants.filter.CONTAINS
+      },
+      columnDefs: [
+        { name: 'name',
+          displayName: 'Pathways',
+          enableFiltering: true,
+          filter: {
+            condition: uiGridConstants.filter.CONTAINS
+          },
+          width: '50%'
+        },
+        { name: 'src',
+          displayName: 'Source',
+          enableFiltering: true,
+          filter: {
+            condition: uiGridConstants.filter.CONTAINS
+          },
+          width: '15%'
+        },
+        { name: 'link',
+          displayName: 'Link',
+          filter: {
+            condition: uiGridConstants.filter.CONTAINS
+          },
+          enableFiltering: true
+        }
+      ]
+    };
+
+    $scope.pathwaysGridOptions.data = data.pathway;
+
+    $scope.geneDetails = data;
+    $scope.done = function(){
+      $modalInstance.close($scope.data);
+    };
   }
 })();
