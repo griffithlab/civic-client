70f36f3de04dedb83f40b5cca54fa9242e55e01b
Joshua McMichael <josh@jmcmichael.com>
2015-04-15 12:50:13 -0500

    variant group summary description displaying properly

--- src/components/services/VariantGroupsService.js
+++ src/components/services/VariantGroupsService.js
@@ -1,10 +1,11 @@
 (function() {
   'use strict';
   angular.module('civic.services')
+    .factory('VariantGroupsResource', VariantGroupsResource)
     .factory('VariantGroups', VariantGroupsService);
 
   // @ngInject
-  function VariantGroupsService($resource) {
+  function VariantGroupsResource($resource) {
     var cacheInterceptor = {
       response: function(response) {
         cache.remove(response.config.url);
@@ -32,6 +33,30 @@
       });
 
     return VariantGroups;
+  }
+
+  // @ngInject
+  function VariantGroupsService(VariantGroupsResource) {
+    return {
+      get: function (variantGroupId) {
+        return VariantGroupsResource.get({variantGroupId: variantGroupId}).$promise
+          .then(function (response) {
+            return response;
+          });
+      },
+      query: function () {
+        return VariantGroupsResource.query().$promise
+          .then(function (response) {
+            return response;
+          });
+      },
+      update: function (reqObj) {
+        return VariantGroupsResource.update(reqObj).$promise
+          .then(function (response) {
+            return response;
+          });
+      }
+    }
   }
 
 })();
