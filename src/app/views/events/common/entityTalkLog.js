f149d3a1effc3cdc4303aa4ef0656e0870ccd825
Joshua McMichael <jmcmicha@genome.wustl.edu>
2015-04-22 17:29:13 -0500

    barebones entity log created

--- src/app/views/events/common/entityTalkLog.js
+++ src/app/views/events/common/entityTalkLog.js
@@ -23,8 +23,45 @@
   }
 
   // @ngInject
-  function EntityTalkLogController($scope) {
+  function EntityTalkLogController($scope, _) {
+    var ctrl,
+      comments,
+      changes,
+      revisions,
+      entityTalkModel;
 
+    entityTalkModel = $scope.entityTalkModel;
+    ctrl = $scope.ctrl = {};
+
+    comments = entityTalkModel.data.comments;
+    revisions = entityTalkModel.data.revisions;
+    changes = entityTalkModel.data.changes;
+
+    _.each(comments, function(comment) {
+      comment.type = 'comment';
+    });
+
+    _.each(revisions, function(revision) {
+      revision.type = 'revision';
+    });
+
+    _.each(changes, function(change) {
+      change.type = 'change';
+    });
+
+    // concatenate event arrays, sort by date, descending
+    ctrl.logItems = comments.concat(revisions, changes);
+    ctrl.logItems = _.chain(ctrl.logItems)
+      .map(function(item) {
+        item.username = typeof item.user === 'object' ? item.user.username : item.user;
+        item.timestamp = _.has(item, 'updated_at') ? item.updated_at : item.created_at;
+        return item;
+      })
+      .sortBy(function(item) {
+        return new Date(item.timestamp);
+      })
+      .reverse()
+      .value();
   }
 
 
