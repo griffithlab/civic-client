54786f41b525cb63929e61c67060c58db87d6fdb
Joshua McMichael <josh@jmcmichael.com>
2015-04-26 16:45:50 -0500

    fixed bug in entity comment form where submit action fails after returning to a talk comment page

--- src/app/views/events/common/entityCommentForm.js
+++ src/app/views/events/common/entityCommentForm.js
@@ -8,58 +8,70 @@
   function entityCommenntFormDirective() {
     return {
       restrict: 'E',
-      scope: {
-        entityTalkModel: '='
-      },
+      scope: {},
+      require: '^^entityTalkView',
+      link: entityCommentFormLink,
       controller: 'EntityCommentFormController',
       templateUrl: 'app/views/events/common/entityCommentForm.tpl.html'
     }
   }
 
   // @ngInject
+  function entityCommentFormLink(scope, element, attrs, entityTalkView) {
+    scope.entityTalkModel = entityTalkView.entityTalkModel;
+  }
+
+  // @ngInject
   function EntityCommentFormController($scope, Security) {
     var ctrl = $scope.ctrl = {};
-    ctrl.isAuthenticated = Security.isAuthenticated();
-    ctrl.isAdmin = Security.isAdmin();
-    ctrl.currentUser = Security.currentUser;
-    ctrl.entityTalkModel = $scope.entityTalkModel;
-    ctrl.newComment = {
-      title: '',
-      text: ''
-    };
 
-    ctrl.newCommentFields = [
-      {
-        key: 'title',
-        type: 'input',
-        templateOptions: {
-          label: 'Title',
-          value: ctrl.newComment.title
+    $scope.$watch('entityTalkModel', function(entityTalkModel) {
+      ctrl.isAuthenticated = Security.isAuthenticated();
+      ctrl.isAdmin = Security.isAdmin();
+      ctrl.currentUser = Security.currentUser;
+
+      ctrl.entityTalkModel = entityTalkModel;
+
+      ctrl.newComment = {
+        title: '',
+        text: ''
+      };
+
+      ctrl.newCommentFields = [
+        {
+          key: 'title',
+          type: 'input',
+          templateOptions: {
+            label: 'Title',
+            value: ctrl.newComment.title
+          }
+        },
+        {
+          key: 'text',
+          type: 'textarea',
+          templateOptions: {
+            label: 'Comment',
+            rows: 5,
+            value: ctrl.newComment.text
+          }
         }
-      },
-      {
-        key: 'text',
-        type: 'textarea',
-        templateOptions: {
-          label: 'Comment',
-          rows: 5,
-          value: ctrl.newComment.text
-        }
-      }
-    ];
+      ];
 
-    ctrl.clear = function() {
+      ctrl.clear = function() {
 
-    };
+      };
 
-    ctrl.submit = function(comment, resetModel) {
-      ctrl.entityTalkModel.actions.submitComment(comment).then(function() {
-        console.log('comment submitted.');
-        ctrl.entityTalkModel.actions.getComments().then(function(response) {
-          console.log(response);
-          resetModel();
+      ctrl.submit = function(comment, resetModel) {
+        ctrl.entityTalkModel.actions.submitComment(comment).then(function() {
+          console.log('comment submitted.');
+          ctrl.entityTalkModel.actions.getComments().then(function(response) {
+            console.log(response);
+            resetModel();
+          });
         });
-      });
-    };
+      };
+    });
+
+
   }
 })();
