0eb7be8991b67ccf645799333366778fe105f09c
Joshua McMichael <josh@jmcmichael.com>
2015-05-03 19:07:33 -0500

    entity tabs now properly style themselves on state reload

--- src/app/views/events/common/entityTabs.js
+++ src/app/views/events/common/entityTabs.js
@@ -68,55 +68,57 @@
 
 // @ngInject
   function EntityTabsController($scope, $state) {
-    var currentStateEqualTo = function (tab) {
+    // TODO not sure why this watch is necessary for tabs to be properly set to active on 1st load
+    var unwatch = $scope.$watchCollection('entityViewModel', function(vm) {
+      var currentStateEqualTo = function (tab) {
+        var isEqual = $state.is(tab.route, tab.params, tab.options);
+        return isEqual;
+      };
 
-      var isEqual = $state.is(tab.route, tab.params, tab.options);
-      return isEqual;
-    };
+      $scope.go = function (tab) {
 
-    $scope.go = function (tab) {
+        if (!currentStateEqualTo(tab) && !tab.disabled) {
+          var promise = $state.go(tab.route, tab.params, tab.options);
 
-      if (!currentStateEqualTo(tab) && !tab.disabled) {
-        var promise = $state.go(tab.route, tab.params, tab.options);
+          /* until the $stateChangeCancel event is released in ui-router, will use this to update
+           tabs if the $stateChangeEvent is cancelled before it finishes loading the state, see
+           https://github.com/rpocklin/ui-router-tabs/issues/19 and
+           https://github.com/angular-ui/ui-router/pull/1090 for further information and
+           https://github.com/angular-ui/ui-router/pull/1844
 
-        /* until the $stateChangeCancel event is released in ui-router, will use this to update
-         tabs if the $stateChangeEvent is cancelled before it finishes loading the state, see
-         https://github.com/rpocklin/ui-router-tabs/issues/19 and
-         https://github.com/angular-ui/ui-router/pull/1090 for further information and
-         https://github.com/angular-ui/ui-router/pull/1844
+           $stateChangeCancel is better since it will handle ui-sref and external $state.go(..) calls */
+          promise.catch(function () {
+            $scope.update_tabs();
+          });
+        }
+      };
 
-         $stateChangeCancel is better since it will handle ui-sref and external $state.go(..) calls */
-        promise.catch(function () {
-          $scope.update_tabs();
+      /* whether to highlight given route as part of the current state */
+      $scope.active = function (tab) {
+        var route = tab.route;
+        // TODO: this is a kludge to fix misbehaving sub-tabs that we have in talk views, almost certainly not the most elegant solution.
+        if (_.contains(tab.route, 'talk')) {
+          // drop the last route element so all talk ancestor $state.includes() evaluates to true
+          route = _.chain(tab.route.split('.')).dropRight().value().join('.');
+        }
+        var isAncestorOfCurrentRoute = $state.includes(route, tab.params, tab.options);
+        return isAncestorOfCurrentRoute;
+      };
+
+      $scope.update_tabs = function () {
+
+        // sets which tab is active (used for highlighting)
+        angular.forEach($scope.tabs, function (tab) {
+          tab.params = tab.params || {};
+          tab.options = tab.options || {};
+          tab.active = $scope.active(tab);
         });
-      }
-    };
+      };
 
-    /* whether to highlight given route as part of the current state */
-    $scope.active = function (tab) {
-      var route = tab.route;
-      // TODO: this is a kludge to fix misbehaving sub-tabs that we have in talk views, almost certainly not the most elegant solution.
-      if (_.contains(tab.route, 'talk')) {
-        // drop the last route element so all talk ancestor $state.includes() evaluates to true
-        route = _.chain(tab.route.split('.')).dropRight().value().join('.');
-      }
-      var isAncestorOfCurrentRoute = $state.includes(route, tab.params, tab.options);
-      return isAncestorOfCurrentRoute;
-    };
-
-    $scope.update_tabs = function () {
-
-      // sets which tab is active (used for highlighting)
-      angular.forEach($scope.tabs, function (tab) {
-        tab.params = tab.params || {};
-        tab.options = tab.options || {};
-        tab.active = $scope.active(tab);
-      });
-    };
-
-    // this always selects the first tab currently - fixed in ui-bootstrap master but not yet released
-    // see https://github.com/angular-ui/bootstrap/commit/91b5fb62eedbb600d6a6abe32376846f327a903d
-    $scope.update_tabs();
-
+      // this always selects the first tab currently - fixed in ui-bootstrap master but not yet released
+      // see https://github.com/angular-ui/bootstrap/commit/91b5fb62eedbb600d6a6abe32376846f327a903d
+      $scope.update_tabs();
+      unwatch();
+    });
   }
 })();
