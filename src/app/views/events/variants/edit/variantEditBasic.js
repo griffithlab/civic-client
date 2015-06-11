59e7ee92a25d72c4a5ecae532a2faa331a36a4b2
Joshua McMichael <jmcmicha@genome.wustl.edu>
2015-06-11 14:06:23 -0500

    added coords to variant edit form

--- src/app/views/events/variants/edit/variantEditBasic.js
+++ src/app/views/events/variants/edit/variantEditBasic.js
@@ -77,6 +77,112 @@
         }
       },
       {
+        template: '<h3 class="form-subheader">Coordinates</h3><hr/>'
+      },
+      {
+        model: vm.variantEdit.coordinates,
+        key: 'chromosome',
+        type: 'horizontalInputHelp',
+        templateOptions: {
+          label: 'Chromosome',
+          value: vm.variantEdit.coordinates.chromosome,
+          helpText: 'Chromosome in which this variant occurs'
+        }
+      },
+      {
+        model: vm.variantEdit.coordinates,
+        key: 'start',
+        type: 'horizontalInputHelp',
+        templateOptions: {
+          label: 'Start',
+          value: vm.variantEdit.coordinates.start,
+          helpText: ''
+        }
+      },
+      {
+        model: vm.variantEdit.coordinates,
+        key: 'stop',
+        type: 'horizontalInputHelp',
+        templateOptions: {
+          label: 'Stop',
+          value: vm.variantEdit.coordinates.stop,
+          helpText: ''
+        }
+      },
+      {
+        model: vm.variantEdit.coordinates,
+        key: 'reference_bases',
+        type: 'horizontalInputHelp',
+        templateOptions: {
+          label: 'Reference Bases',
+          value: vm.variantEdit.coordinates.reference_bases,
+          helpText: ''
+        }
+      },
+      {
+        model: vm.variantEdit.coordinates,
+        key: 'variant_bases',
+        type: 'horizontalInputHelp',
+        templateOptions: {
+          label: 'Variant Bases',
+          value: vm.variantEdit.coordinates.variant_bases,
+          helpText: ''
+        }
+      },
+      {
+        model: vm.variantEdit.coordinates,
+        key: 'representative_transcript',
+        type: 'horizontalInputHelp',
+        templateOptions: {
+          label: 'Representative Transcript',
+          value: vm.variantEdit.coordinates.representative_transcript,
+          helpText: ''
+        }
+      },
+      {
+        model: vm.variantEdit.coordinates,
+        key: 'reference_bases',
+        type: 'horizontalInputHelp',
+        templateOptions: {
+          label: 'Reference Bases',
+          value: vm.variantEdit.coordinates.reference_bases,
+          helpText: ''
+        }
+      },
+      {
+        template: '<h3 class="form-subheader">Fusion Coordinates</h3><hr/>'
+      },
+      {
+        model: vm.variantEdit.coordinates,
+        key: 'chromosome2',
+        type: 'horizontalInputHelp',
+        templateOptions: {
+          label: 'Chromosome 2',
+          value: vm.variantEdit.coordinates.chromosome2,
+          helpText: 'If variant is a fusion, specify its coordinates'
+        }
+      },
+      {
+        model: vm.variantEdit.coordinates,
+        key: 'start2',
+        type: 'horizontalInputHelp',
+        templateOptions: {
+          label: 'Start 2',
+          value: vm.variantEdit.coordinates.start2,
+          helpText: ''
+        }
+      },
+      {
+        model: vm.variantEdit.coordinates,
+        key: 'stop2',
+        type: 'horizontalInputHelp',
+        templateOptions: {
+          label: 'Stop 2',
+          value: vm.variantEdit.coordinates.stop2,
+          helpText: ''
+        }
+      },
+      {
         template: '<hr/>'
       },
       {
