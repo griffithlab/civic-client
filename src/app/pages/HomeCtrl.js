e69da474fb09b3daebb7ac8601926381ee78da40
Joshua McMichael <jmcmicha@genome.wustl.edu>
2015-07-22 12:41:07 -0500

    added basic event stream display to homepage

--- src/app/pages/HomeCtrl.js
+++ src/app/pages/HomeCtrl.js
@@ -4,11 +4,17 @@
     .controller('HomeCtrl', HomeCtrl);
 
   // @ngInject
-  function HomeCtrl($scope, Stats) {
+  function HomeCtrl($scope, Stats, Events) {
     var vm = $scope.vm = {};
+
     vm.stats = {};
+    vm.events = [];
+
     Stats.get('site').then(function(stats) {
       vm.stats = stats;
+    });
+    Events.query().then(function(events) {
+      vm.events = events;
     });
   }
 })();
