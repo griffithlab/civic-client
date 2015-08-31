9db70068992e1ff3231713c513ab4690eb2cc880
Joshua McMichael <jmcmicha@genome.wustl.edu>
2015-08-31 11:44:39 -0500

    componentized entity events block

---
+++ src/app/views/events/common/actionBlock.js
@@ -0,0 +1,23 @@
+(function() {
+  'use strict';
+  angular.module('civic.events.common')
+  .directive('actionBlock', actionBlockDirective)
+  .controller('ActionBlockController', ActionBlockController);
+
+  // @ngInject
+  function actionBlockDirective() {
+    return /* @ngInject */ {
+      restrict: 'E',
+      scope: {
+        users: '='
+      },
+      controller: 'ActionBlockController',
+      templateUrl: 'app/views/events/common/actionBlock.tpl.html'
+    }
+  }
+
+  // @ngInject
+  function ActionBlockController($scope) {
+    console.log('action block controller loaded.');
+  }
+})();
