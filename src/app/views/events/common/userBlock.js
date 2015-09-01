0baf35f03d7d76ed813e3a84cb9a6d2aee077a2c
Joshua McMichael <jmcmicha@genome.wustl.edu>
2015-09-01 16:24:20 -0500

    added profile-linked user buttons to entity event blocks

---
+++ src/app/views/events/common/userBlock.js
@@ -0,0 +1,24 @@
+(function() {
+  'use strict';
+
+  angular.module('civic.events.common')
+    .directive('userBlock', userBlockDirective)
+    .controller('UserBlockController', UserBlockController);
+
+  // @ngInject
+  function userBlockDirective() {
+    return /* @ngInject */ {
+      restrict: 'E',
+      scope: {
+        user: '='
+      },
+      controller: 'UserBlockController',
+      templateUrl: 'app/views/events/common/userBlock.tpl.html'
+    }
+  }
+
+  // @inject
+  function UserBlockController($scope) {
+    console.log('userBlock controller called.');
+  }
+})();
