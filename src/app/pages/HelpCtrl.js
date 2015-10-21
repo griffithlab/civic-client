a80833a77aad9e7f206b264745194b71db599ca9
Joshua McMichael <jmcmicha@genome.wustl.edu>
2015-10-21 11:44:30 -0500

    refactored help section to add a sidebar menu and additional sections for genes, variants etc.

--- src/app/pages/HelpCtrl.js
+++ src/app/pages/HelpCtrl.js
@@ -6,28 +6,56 @@
   // @ngInject
   function HelpCtrl($scope) {
     var vm = $scope.vm = {};
-    vm.tabs = [
-
-      {
-        heading: 'Evidence Item',
-        template: 'app/pages/help_evidence_statement.tpl.html',
-        active: true
-      },
-      {
-        heading: 'Evidence Types',
-        template: 'app/pages/help_evidence_types.tpl.html',
-        active: false
-      },
-      {
-        heading: 'Evidence Levels',
-        template: 'app/pages/help_evidence_levels.tpl.html',
-        active: false
-      },
-      {
-        heading: 'Trust Ratings',
-        template: 'app/pages/help_trust_ratings.tpl.html',
-        active: false
-      }
-    ];
+    vm.tabs = {
+      main: [
+        {
+          heading: 'Introduction',
+          template: 'app/pages/help_intro.tpl.html',
+          active: true
+        },
+        {
+          heading: 'Evidence Items',
+          template: 'app/pages/help_evidence_main.tpl.html',
+          active: false
+        },
+        {
+          heading: 'Variants',
+          template: 'app/pages/help_variants.tpl.html',
+          active: false
+        },
+        {
+          heading: 'Genes',
+          template: 'app/pages/help_genes.tpl.html',
+          active: false
+        },
+        {
+          heading: 'Variant Groups',
+          template: 'app/pages/help_variant_groups.tpl.html',
+          active: false
+        }
+      ],
+      evidence: [
+        {
+          heading: 'Evidence Item',
+          template: 'app/pages/help_evidence_statement.tpl.html',
+          active: true
+        },
+        {
+          heading: 'Evidence Types',
+          template: 'app/pages/help_evidence_types.tpl.html',
+          active: false
+        },
+        {
+          heading: 'Evidence Levels',
+          template: 'app/pages/help_evidence_levels.tpl.html',
+          active: false
+        },
+        {
+          heading: 'Trust Ratings',
+          template: 'app/pages/help_evidence_trust_ratings.tpl.html',
+          active: false
+        }
+      ]
+    };
   }
 })();
