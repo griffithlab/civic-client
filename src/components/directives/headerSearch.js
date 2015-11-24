19726cba46ecdf6b15730e9777d7b59fffd10ce4
Joshua McMichael <jmcmicha@genome.wustl.edu>
2015-11-24 11:05:33 -0600

    appended Add button to header search buttons

--- src/components/directives/headerSearch.js
+++ src/components/directives/headerSearch.js
@@ -1,14 +1,23 @@
 (function() {
   'use strict';
   angular.module('civic.common')
-    .directive('headerSearch', headerSearch);
+    .directive('headerSearch', headerSearch)
+    .controller('HeaderSearchController', HeaderSearchController);
 
   function headerSearch() {
     return {
       restrict: 'E',
       templateUrl: 'components/directives/headerSearch.tpl.html',
-      controller: headerSearch
+      controller: 'HeaderSearchController'
     };
 
+  }
+
+  // @ngInject
+  function HeaderSearchController($scope, Security) {
+    var vm = $scope.vm = {};
+    vm.isEditor= Security.isEditor;
+    vm.isAuthenticated = Security.isAuthenticated;
+    vm.test = "test";
   }
 })();
