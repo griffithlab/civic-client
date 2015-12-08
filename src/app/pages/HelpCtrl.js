208880779ec0782687a22d9dcd475690f6742867
KK <kkrysiak@genome.wustl.edu>
2015-12-08 14:59:41 -0600

    Creating new tabs in help documentation Variants and Evidence sections

--- src/app/pages/HelpCtrl.js
+++ src/app/pages/HelpCtrl.js
@@ -14,7 +14,7 @@
           active: true
         },
         {
-          heading: 'Evidence Items',
+          heading: 'Evidence',
           template: 'app/pages/help_evidence_main.tpl.html',
           active: false
         },
@@ -51,6 +51,11 @@
           active: true
         },
         {
+          heading: 'Variant Origin',
+          template: 'app/pages/help_evidence_variant_origin.tpl.html',
+          active: false
+        },
+        {
           heading: 'Evidence Types',
           template: 'app/pages/help_evidence_types.tpl.html',
           active: false
@@ -65,7 +70,29 @@
           template: 'app/pages/help_evidence_trust_ratings.tpl.html',
           active: false
         }
-      ]
+      ],
+      variant: [
+        {
+          heading: 'Overview',
+          template: 'app/pages/help_variant_overview.tpl.html',
+          active: true
+        },
+        {
+          heading: 'Variant Naming',
+          template: 'app/pages/help_variant_naming.tpl.html',
+          active: false
+        },
+        {
+          heading: 'Variant Summary',
+          template: 'app/pages/help_variant_summaries.tpl.html',
+          active: false
+        },
+        {
+          heading: 'Variant Coordinates',
+          template: 'app/pages/help_variant_coordinates.tpl.html',
+          active: false
+        },
+      ],
     };
   }
 })();
