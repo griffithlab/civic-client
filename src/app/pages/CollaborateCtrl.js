22fdf8335219dd07f460b3b1de77a4dc02d175b3
Joshua McMichael <jmcmicha@genome.wustl.edu>
2015-12-17 14:40:03 -0600

    added popup zoomed view to collaborate img

--- src/app/pages/CollaborateCtrl.js
+++ src/app/pages/CollaborateCtrl.js
@@ -5,7 +5,16 @@
     .controller('CollaborateCtrl', CollaborateCtrl);
 
 // @ngInject
-  function CollaborateCtrl ($scope) {
-    $scope.loadedMsg = 'Loaded Collaborate!';
+  function CollaborateCtrl ($scope, $modal) {
+    var vm = $scope.vm = {};
+
+    vm.imgPopup = function imgPopup() {
+      $modal.open({
+        animation: false,
+        backdrop: true,
+        template: '<div><img src="assets/images/GP-113_CIViC_schema-collaboration_PROCESS_v1a.png" class="img-fluid" width="100%" height="100%" alt="CIViC Collaboration Diagram"/></div>',
+        size: 'lg'
+      })
+    };
   }
 })();
