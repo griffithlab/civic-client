9c4a1d6a670e893b3d827c580eb3b8ff2590488c
Joshua McMichael <jmcmicha@genome.wustl.edu>
2016-02-12 14:14:06 -0600

    stats and events displays on the homepage now update every 60 seconds (closes #326)

--- src/app/pages/HomeCtrl.js
+++ src/app/pages/HomeCtrl.js
@@ -4,7 +4,7 @@
     .controller('HomeCtrl', HomeCtrl);
 
   // @ngInject
-  function HomeCtrl($scope, Stats, Events) {
+  function HomeCtrl($scope, $interval, Stats, Events) {
     var vm = $scope.vm = {};
 
     vm.stats = {};
@@ -16,5 +16,15 @@
     Events.query({count: 7}).then(function(response) {
       vm.events = response.result;
     });
+
+    // refresh stats and events periodically
+    $interval(function() {
+      Stats.site().then(function(stats) {
+        vm.stats = stats;
+      });
+      Events.query({count: 7}).then(function(response) {
+        vm.events = response.result;
+      });
+    }, 60000);
   }
 })();
