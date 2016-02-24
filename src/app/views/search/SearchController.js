53ac1f60c55b63a93c2334ef91b08f26c3041eb8
Joshua McMichael <jmcmicha@genome.wustl.edu>
2016-02-24 12:51:46 -0600

    added gene form config

--- src/app/views/search/SearchController.js
+++ src/app/views/search/SearchController.js
@@ -230,7 +230,7 @@
                     {value: 'is', name: 'is'},
                     {value: 'contains', name: 'contains'},
                     {value: 'begins_with', name: 'begins with'},
-                    {value: 'does_not_contain', name: 'does not contain'},
+                    {value: 'does_not_contain', name: 'does not contain'}
 
                   ]
                 }
@@ -605,6 +605,540 @@
                 key: 'parameters[0]',
                 type: 'input',
                 className: 'inline-field',
+                templateOptions: {
+                  label: '',
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      }
+    ];
+
+    vm.fields.genes = [
+      {
+        type: 'queryRow',
+        key: 'queries',
+        templateOptions: {
+          rowFields: [
+            {
+              key: 'field',
+              type: 'queryBuilderSelect',
+              templateOptions: {
+                label: '',
+                required: true,
+                options: [
+                  { value: '', name: 'Please select a field' },
+                  { value: 'entrez_id', name: 'Entrez ID' },
+                  { value: 'name', name: 'Name' },
+                  { value: 'aliases', name: 'Aliases' },
+                  { value: 'description', name: 'Description' }
+                ],
+                onChange: function(value, options, scope) {
+                  scope.model.condition = {
+                    name: undefined,
+                    parameters: []
+                  };
+                }
+              }
+            }
+          ],
+          conditionFields: {
+            entrez_id: [
+              {
+                key: 'name',
+                type: 'queryBuilderSelect',
+                className: 'inline-field inline-field-md',
+                data: {
+                  defaultValue: 'is'
+                },
+                templateOptions: {
+                  label: '',
+                  required: true,
+                  options: [
+                    {value: 'is', name: 'is'},
+                    {value: 'is_not', name: 'is not'}
+                  ]
+                }
+              },
+              {
+                key: 'parameters[0]', // entrez_id
+                type: 'input',
+                className: 'inline-field',
+                templateOptions: {
+                  label: '',
+                  required: true
+                }
+              }
+            ],
+            name: [
+              {
+                key: 'name',
+                type: 'queryBuilderSelect',
+                className: 'inline-field',
+                data: {
+                  defaultValue: 'contains'
+                },
+                templateOptions: {
+                  label: '',
+                  required: true,
+                  options: [
+                    {value: 'contains', name: 'contains'},
+                    {value: 'begins_with', name: 'begins with'},
+                    {value: 'does_not_contain', name: 'does not contain'},
+                    {value: 'is_empty', name: 'is empty'}
+                  ]
+                }
+              },
+              {
+                key: 'parameters[0]',
+                type: 'input',
+                className: 'inline-field',
+                hideExpression: 'model.name === "is_empty"',
+                templateOptions: {
+                  label: '',
+                  required: true
+                }
+              }
+            ],
+            aliases: [
+              {
+                key: 'name',
+                type: 'queryBuilderSelect',
+                className: 'inline-field',
+                data: {
+                  defaultValue: 'contains'
+                },
+                templateOptions: {
+                  label: '',
+                  required: true,
+                  options: [
+                    {value: 'contains', name: 'contains'},
+                    {value: 'begins_with', name: 'begins with'},
+                    {value: 'does_not_contain', name: 'does not contain'},
+                    {value: 'is_empty', name: 'is empty'}
+                  ]
+                }
+              },
+              {
+                key: 'parameters[0]',
+                type: 'input',
+                className: 'inline-field',
+                hideExpression: 'model.name === "is_empty"',
+                templateOptions: {
+                  label: '',
+                  required: true
+                }
+              }
+            ],
+            description: [
+              {
+                key: 'name',
+                type: 'queryBuilderSelect',
+                className: 'inline-field',
+                data: {
+                  defaultValue: 'contains'
+                },
+                templateOptions: {
+                  label: '',
+                  required: true,
+                  options: [
+                    {value: 'contains', name: 'contains'},
+                    {value: 'begins_with', name: 'begins with'},
+                    {value: 'does_not_contain', name: 'does not contain'},
+                    {value: 'is_empty', name: 'is empty'}
+                  ]
+                }
+              },
+              {
+                key: 'parameters[0]',
+                type: 'input',
+                className: 'inline-field',
+                hideExpression: 'model.name === "is_empty"',
+                templateOptions: {
+                  label: '',
+                  required: true
+                }
+              }
+            ]
+          }
+        }
+      }
+    ];
+
+    vm.fields.variants = [
+      {
+        type: 'queryRow',
+        key: 'queries',
+        templateOptions: {
+          rowFields: [
+            {
+              key: 'field',
+              type: 'queryBuilderSelect',
+              templateOptions: {
+                label: '',
+                required: true,
+                options: [
+                  { value: '', name: 'Please select a field' },
+                  { value: 'name', name: 'Name' },
+                  { value: 'description', name: 'Description' },
+                  { value: 'reference_build', name: 'Reference Build' },
+                  { value: 'ensembl_version', name: 'Ensembl Version' },
+                  { value: 'reference_bases', name: 'Reference Base(s)' },
+                  { value: 'variant_bases', name: 'Variant Base(s)' },
+                  { value: 'chromosome', name: 'CHR1 Chromosome' },
+                  { value: 'start', name: 'CHR1 Start' },
+                  { value: 'stop', name: 'CHR1 Stop' },
+                  { value: 'representative_transcript', name: 'CHR1 Representative Transcript' },
+                  { value: 'chromosome2', name: 'CHR2 Chromosome' },
+                  { value: 'start2', name: 'CHR2 Start' },
+                  { value: 'stop2', name: 'CHR2 Stop' },
+                  { value: 'representative_transcript2', name: 'CHR2 Representative Transcript' }                     ],
+                onChange: function(value, options, scope) {
+                  scope.model.condition = {
+                    name: undefined,
+                    parameters: []
+                  };
+                }
+              }
+            }
+          ],
+          conditionFields: {
+            name: [
+              {
+                key: 'name',
+                type: 'queryBuilderSelect',
+                className: 'inline-field',
+                data: {
+                  defaultValue: 'contains'
+                },
+                templateOptions: {
+                  label: '',
+                  required: true,
+                  options: [
+                    {value: 'contains', name: 'contains'},
+                    {value: 'begins_with', name: 'begins with'},
+                    {value: 'does_not_contain', name: 'does not contain'},
+                    {value: 'is_empty', name: 'is empty'}
+                  ]
+                }
+              },
+              {
+                key: 'parameters[0]',
+                type: 'input',
+                className: 'inline-field',
+                hideExpression: 'model.name === "is_empty"',
+                templateOptions: {
+                  label: '',
+                  required: true
+                }
+              }
+            ],
+            description: [
+              {
+                key: 'name',
+                type: 'queryBuilderSelect',
+                className: 'inline-field',
+                data: {
+                  defaultValue: 'contains'
+                },
+                templateOptions: {
+                  label: '',
+                  required: true,
+                  options: [
+                    {value: 'contains', name: 'contains'},
+                    {value: 'begins_with', name: 'begins with'},
+                    {value: 'does_not_contain', name: 'does not contain'},
+                    {value: 'is_empty', name: 'is empty'}
+                  ]
+                }
+              },
+              {
+                key: 'parameters[0]',
+                type: 'input',
+                className: 'inline-field',
+                hideExpression: 'model.name === "is_empty"',
+                templateOptions: {
+                  label: '',
+                  required: true
+                }
+              }
+            ],
+            reference_build: [
+              {
+                key: 'name',
+                type: 'queryBuilderSelect',
+                className: 'inline-field inline-field-md',
+                data: {
+                  defaultValue: 'is_equal_to'
+                },
+                templateOptions: {
+                  label: '',
+                  required: true,
+                  options: [
+                    {value: 'is_equal_to', name: 'is'},
+                    {value: 'is_not_equal_to', name: 'is not'}
+                  ]
+                }
+              },
+              {
+                key: 'parameters[0]',
+                type: 'queryBuilderSelect',
+                className: 'inline-field',
+                data: {
+                  defaultValue: 'GRCh38'
+                },
+                templateOptions: {
+                  label: '',
+                  required: true,
+                  options: [
+                    { value: 'GRCh38', name: 'GRCh38 (hg20)' },
+                    { value: 'GRCh37', name: 'GRCh37 (hg19)' },
+                    { value: 'NCBI36', name: 'NCBI36 (hg18)' }
+                  ]
+                }
+              }
+            ],
+            ensembl_version: [
+              {
+                key: 'name',
+                type: 'queryBuilderSelect',
+                className: 'inline-field',
+                data: {
+                  defaultValue: 'is_equal_to'
+                },
+                templateOptions: {
+                  label: '',
+                  required: true,
+                  options: [
+                    {value: 'is_equal_to', name: 'is'},
+                    {value: 'is_not_equal_to', name: 'is not'}
+                  ]
+                }
+              },
+              {
+                key: 'parameters[0]',
+                type: 'input',
+                className: 'inline-field',
+                hideExpression: 'model.name === "is_empty"',
+                templateOptions: {
+                  label: '',
+                  required: true
+                }
+              }
+            ],
+            reference_bases: [
+              {
+                key: 'name',
+                type: 'queryBuilderSelect',
+                className: 'inline-field',
+                data: {
+                  defaultValue: 'contains'
+                },
+                templateOptions: {
+                  label: '',
+                  required: true,
+                  options: [
+                    {value: 'contains', name: 'contains'},
+                    {value: 'begins_with', name: 'begins with'},
+                    {value: 'does_not_contain', name: 'does not contain'},
+                    {value: 'is_empty', name: 'is empty'}
+                  ]
+                }
+              },
+              {
+                key: 'parameters[0]',
+                type: 'input',
+                className: 'inline-field',
+                hideExpression: 'model.name === "is_empty"',
+                templateOptions: {
+                  label: '',
+                  required: true
+                }
+              }
+            ],
+            variant_bases: [
+              {
+                key: 'name',
+                type: 'queryBuilderSelect',
+                className: 'inline-field',
+                data: {
+                  defaultValue: 'contains'
+                },
+                templateOptions: {
+                  label: '',
+                  required: true,
+                  options: [
+                    {value: 'contains', name: 'contains'},
+                    {value: 'begins_with', name: 'begins with'},
+                    {value: 'does_not_contain', name: 'does not contain'},
+                    {value: 'is_empty', name: 'is empty'}
+                  ]
+                }
+              },
+              {
+                key: 'parameters[0]',
+                type: 'input',
+                className: 'inline-field',
+                hideExpression: 'model.name === "is_empty"',
+                templateOptions: {
+                  label: '',
+                  required: true
+                }
+              }
+            ],
+            chromosome: [
+              {
+                key: 'name',
+                type: 'queryBuilderSelect',
+                className: 'inline-field inline-field-md',
+                data: {
+                  defaultValue: 'is_equal_to'
+                },
+                templateOptions: {
+                  label: '',
+                  required: true,
+                  options: [
+                    {value: 'is_equal_to', name: 'is'},
+                    {value: 'is_not_equal_to', name: 'is not'},
+                    {value: 'is_empty', name: 'is empty'}
+                  ]
+                }
+              },
+              {
+                key: 'parameters[0]',
+                type: 'input',
+                className: 'inline-field',
+                hideExpression: 'model.name === "is_empty"',
+                templateOptions: {
+                  label: '',
+                  required: true
+                }
+              }
+            ],
+            start: [
+              {
+                key: 'name',
+                type: 'queryBuilderSelect',
+                className: 'inline-field',
+                data: {
+                  defaultValue: 'is_greater_than_or_equal_to'
+                },
+                templateOptions: {
+                  required: true,
+                  label: '',
+                  options: [
+                    { value: 'is_greater_than_or_equal_to', name: 'is greater than or equal to' },
+                    { value: 'is_greater_than', name: 'is greater than' },
+                    { value: 'is_less_than', name: 'is less than' },
+                    { value: 'is_less_than_or_equal_to', name: 'is less than or equal to' },
+                    { value: 'is_equal_to', name: 'is equal to' },
+                    { value: 'is_in_the_range', name: 'is in the range'}
+                  ],
+                  onChange: function(value, options, scope) {
+                    _.pullAt(scope.model.parameters, 1,2);
+                  }
+                }
+              },
+              {
+                key: 'parameters[0]',
+                type: 'input',
+                className: 'inline-field',
+                templateOptions: {
+                  label: '',
+                  required: true
+                }
+              },
+              {
+                template: 'to',
+                className: 'inline-field',
+                hideExpression: 'model.name != "is_in_the_range"'
+              },
+              {
+                key: 'parameters[1]',
+                type: 'input',
+                className: 'inline-field',
+                hideExpression: 'model.name != "is_in_the_range"',
+                templateOptions: {
+                  label: '',
+                  required: true
+                }
+              }
+            ],
+            stop: [
+              {
+                key: 'name',
+                type: 'queryBuilderSelect',
+                className: 'inline-field',
+                data: {
+                  defaultValue: 'is_greater_than_or_equal_to'
+                },
+                templateOptions: {
+                  required: true,
+                  label: '',
+                  options: [
+                    { value: 'is_greater_than_or_equal_to', name: 'is greater than or equal to' },
+                    { value: 'is_greater_than', name: 'is greater than' },
+                    { value: 'is_less_than', name: 'is less than' },
+                    { value: 'is_less_than_or_equal_to', name: 'is less than or equal to' },
+                    { value: 'is_equal_to', name: 'is equal to' },
+                    { value: 'is_in_the_range', name: 'is in the range'}
+                  ],
+                  onChange: function(value, options, scope) {
+                    _.pullAt(scope.model.parameters, 1,2);
+                  }
+                }
+              },
+              {
+                key: 'parameters[0]',
+                type: 'input',
+                className: 'inline-field',
+                templateOptions: {
+                  label: '',
+                  required: true
+                }
+              },
+              {
+                template: 'to',
+                className: 'inline-field',
+                hideExpression: 'model.name != "is_in_the_range"'
+              },
+              {
+                key: 'parameters[1]',
+                type: 'input',
+                className: 'inline-field',
+                hideExpression: 'model.name != "is_in_the_range"',
+                templateOptions: {
+                  label: '',
+                  required: true
+                }
+              }
+            ],
+            representative_transcript: [
+              {
+                key: 'name',
+                type: 'queryBuilderSelect',
+                className: 'inline-field',
+                data: {
+                  defaultValue: 'contains'
+                },
+                templateOptions: {
+                  label: '',
+                  required: true,
+                  options: [
+                    {value: 'contains', name: 'contains'},
+                    {value: 'begins_with', name: 'begins with'},
+                    {value: 'does_not_contain', name: 'does not contain'},
+                    {value: 'is_empty', name: 'is empty'}
+                  ]
+                }
+              },
+              {
+                key: 'parameters[0]',
+                type: 'input',
+                className: 'inline-field',
+                hideExpression: 'model.name === "is_empty"',
                 templateOptions: {
                   label: '',
                   required: true
