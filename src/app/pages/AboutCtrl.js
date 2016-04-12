e2c9e80b7336e3044e88e4b6b8c6517cf5c72485
Joshua McMichael <jmcmicha@genome.wustl.edu>
2016-04-12 13:49:57 -0500

    added domain experts list w/ avatars, descrition, profile info on About page (closes #338)

--- src/app/pages/AboutCtrl.js
+++ src/app/pages/AboutCtrl.js
@@ -5,118 +5,64 @@
     .controller('AboutCtrl', AboutCtrl);
 
 // @ngInject
-  function AboutCtrl ($scope) {
+  function AboutCtrl ($scope, $state) {
     var vm = $scope.vm = {};
 
-    vm.contributors = [{
-      'id': 103,
-      'email': 'user_1@example.com',
-      'name': null,
-      'last_seen_at': null,
-      'username': 'User 1',
-      'role': 'curator',
-      'avatar_url': 'https://secure.gravatar.com/avatar/be72a6287c41316db75b7bc8020024c8.png?d=identicon&r=pg&s=32',
-      'avatars': {
-        'x128': 'https://secure.gravatar.com/avatar/be72a6287c41316db75b7bc8020024c8.png?d=identicon&r=pg&s=128',
-        'x64': 'https://secure.gravatar.com/avatar/be72a6287c41316db75b7bc8020024c8.png?d=identicon&r=pg&s=64',
-        'x32': 'https://secure.gravatar.com/avatar/be72a6287c41316db75b7bc8020024c8.png?d=identicon&r=pg&s=32',
-        'x14': 'https://secure.gravatar.com/avatar/be72a6287c41316db75b7bc8020024c8.png?d=identicon&r=pg&s=14'
-      },
-      'area_of_expertise': null,
-      'orcid': null,
-      'display_name': 'User 1',
-      'community_params': {
-        'most_recent_action_timestamp': null,
-        'action_count': 0
-      }
-    },
+    vm.experts = [
       {
-        'id': 104,
-        'email': 'user_2@example.com',
-        'name': null,
-        'last_seen_at': null,
-        'username': 'User 2',
-        'role': 'curator',
-        'avatar_url': 'https://secure.gravatar.com/avatar/de54c9567234025a4b326d7882e91ce4.png?d=identicon&r=pg&s=32',
-        'avatars': {
-          'x128': 'https://secure.gravatar.com/avatar/de54c9567234025a4b326d7882e91ce4.png?d=identicon&r=pg&s=128',
-          'x64': 'https://secure.gravatar.com/avatar/de54c9567234025a4b326d7882e91ce4.png?d=identicon&r=pg&s=64',
-          'x32': 'https://secure.gravatar.com/avatar/de54c9567234025a4b326d7882e91ce4.png?d=identicon&r=pg&s=32',
-          'x14': 'https://secure.gravatar.com/avatar/de54c9567234025a4b326d7882e91ce4.png?d=identicon&r=pg&s=14'
-        },
-        'area_of_expertise': null,
-        'orcid': null,
-        'display_name': 'User 2',
-        'community_params': {
-          'most_recent_action_timestamp': null,
-          'action_count': 0
-        }
+        'id': 133,
+        'name': 'Elaine Mardis',
+        'role': 'Curator',
+        'avatar_url': 'assets/images/experts/emardis.jpg',
+        'area_of_expertise': 'Research Scientist',
+        'display_name': 'emardis',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla luctus, odio non cursus maximus, justo elit malesuada libero, non elementum purus nulla vitae ipsum. Morbi in mattis elit.'
       },
       {
-        'id': 105,
-        'email': 'user_3@example.com',
-        'name': null,
-        'last_seen_at': null,
-        'username': 'User 3',
-        'role': 'curator',
-        'avatar_url': 'https://secure.gravatar.com/avatar/64bf6b54ba4b065b5f45e5187756870a.png?d=identicon&r=pg&s=32',
-        'avatars': {
-          'x128': 'https://secure.gravatar.com/avatar/64bf6b54ba4b065b5f45e5187756870a.png?d=identicon&r=pg&s=128',
-          'x64': 'https://secure.gravatar.com/avatar/64bf6b54ba4b065b5f45e5187756870a.png?d=identicon&r=pg&s=64',
-          'x32': 'https://secure.gravatar.com/avatar/64bf6b54ba4b065b5f45e5187756870a.png?d=identicon&r=pg&s=32',
-          'x14': 'https://secure.gravatar.com/avatar/64bf6b54ba4b065b5f45e5187756870a.png?d=identicon&r=pg&s=14'
-        },
-        'area_of_expertise': null,
-        'orcid': null,
-        'display_name': 'User 3',
-        'community_params': {
-          'most_recent_action_timestamp': null,
-          'action_count': 0
-        }
+        'id': 144,
+        'name': 'Ron Bose',
+        'role': 'Curator',
+        'avatar_url': 'assets/images/experts/rbose.jpg',
+        'area_of_expertise': 'Research Scientist',
+        'display_name': 'rbose',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla luctus, odio non cursus maximus, justo elit malesuada libero, non elementum purus nulla vitae ipsum. Morbi in mattis elit.'
       },
       {
-        'id': 106,
-        'email': 'user_4@example.com',
-        'name': null,
-        'last_seen_at': null,
-        'username': 'User 4',
-        'role': 'curator',
-        'avatar_url': 'https://secure.gravatar.com/avatar/1d0a8474488144f248722d20e9fd0e24.png?d=identicon&r=pg&s=32',
-        'avatars': {
-          'x128': 'https://secure.gravatar.com/avatar/1d0a8474488144f248722d20e9fd0e24.png?d=identicon&r=pg&s=128',
-          'x64': 'https://secure.gravatar.com/avatar/1d0a8474488144f248722d20e9fd0e24.png?d=identicon&r=pg&s=64',
-          'x32': 'https://secure.gravatar.com/avatar/1d0a8474488144f248722d20e9fd0e24.png?d=identicon&r=pg&s=32',
-          'x14': 'https://secure.gravatar.com/avatar/1d0a8474488144f248722d20e9fd0e24.png?d=identicon&r=pg&s=14'
-        },
-        'area_of_expertise': null,
-        'orcid': null,
-        'display_name': 'User 4',
-        'community_params': {
-          'most_recent_action_timestamp': null,
-          'action_count': 0
-        }
+        'id': 15,
+        'name': 'Malachi Griffith',
+        'username': 'malachigriffith',
+        'role': 'Curator',
+        'avatar_url': 'assets/images/experts/mgriffith.jpg',
+        'area_of_expertise': 'Research Scientist',
+        'display_name': 'malachigriffith',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla luctus, odio non cursus maximus, justo elit malesuada libero, non elementum purus nulla vitae ipsum. Morbi in mattis elit.'
       },
       {
-        'id': 107,
-        'email': 'user_5@example.com',
-        'name': null,
-        'last_seen_at': null,
-        'username': 'User 5',
-        'role': 'curator',
-        'avatar_url': 'https://secure.gravatar.com/avatar/2ff2d013dd15c65ee144645b507a6542.png?d=identicon&r=pg&s=32',
-        'avatars': {
-          'x128': 'https://secure.gravatar.com/avatar/2ff2d013dd15c65ee144645b507a6542.png?d=identicon&r=pg&s=128',
-          'x64': 'https://secure.gravatar.com/avatar/2ff2d013dd15c65ee144645b507a6542.png?d=identicon&r=pg&s=64',
-          'x32': 'https://secure.gravatar.com/avatar/2ff2d013dd15c65ee144645b507a6542.png?d=identicon&r=pg&s=32',
-          'x14': 'https://secure.gravatar.com/avatar/2ff2d013dd15c65ee144645b507a6542.png?d=identicon&r=pg&s=14'
-        },
-        'area_of_expertise': null,
-        'orcid': null,
-        'display_name': 'User 5',
-        'community_params': {
-          'most_recent_action_timestamp': null,
-          'action_count': 0
-        }
+        'id': 6,
+        'name': 'Kilannin Krysiak',
+        'role': 'Curator',
+        'avatar_url': 'assets/images/experts/kkrysiak.jpg',
+        'area_of_expertise': 'Research Scientist',
+        'display_name': 'kkrysiak',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla luctus, odio non cursus maximus, justo elit malesuada libero, non elementum purus nulla vitae ipsum. Morbi in mattis elit.'
+      },
+      {
+        'id': 41,
+        'name': 'Nick Spies',
+        'role': 'Curator',
+        'avatar_url': 'assets/images/experts/nspies.jpg',
+        'area_of_expertise': 'Research Scientist',
+        'display_name': 'nickspies',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla luctus, odio non cursus maximus, justo elit malesuada libero, non elementum purus nulla vitae ipsum. Morbi in mattis elit.'
+      },
+      {
+        'id': 3,
+        'name': 'Obi Griffith',
+        'role': 'Curator',
+        'avatar_url': 'assets/images/experts/ogriffith.jpg',
+        'area_of_expertise': 'Research Scientist',
+        'display_name': 'ogriffith',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla luctus, odio non cursus maximus, justo elit malesuada libero, non elementum purus nulla vitae ipsum. Morbi in mattis elit.'
       }
     ];
   }
