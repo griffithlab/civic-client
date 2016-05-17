e3e826be2f2f35ff84dd5b639b7511a02810724c
Joshua McMichael <jmcmicha@genome.wustl.edu>
2016-05-17 10:37:52 -0500

    added instructions to point the dev gulp server to the live API instead of the local API

--- gulp/server.js
+++ gulp/server.js
@@ -23,6 +23,9 @@ function connectInit(baseDir, livereload) {
     middleware: function() {
       return [ (function() {
         var options = url.parse('http://127.0.0.1:3000/api');
+        // comment out the line above, and uncomment the line below to query the
+        // production API. Note that login will not work
+        // var options = url.parse('http://civic.genome.wustl.edu/api');
         options.route = '/api';
         return proxy(options);
       })() ];
