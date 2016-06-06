16c65c297cfdcc2115a9838f35796c83e7b77a19
agraubert <captianjroot@live.com>
2016-06-06 14:47:13 -0500

    Added individual tests for each cdnized dependency

--- gulp/cdnize.js
+++ gulp/cdnize.js
@@ -10,19 +10,10 @@ gulp.task('cdnize', ['wiredep'], function () {
     allowRev: false, //stricter version matching
     allowMin: true, //request minified files
     relativeRoot: 'src/', //asset root directory
+    fallbackTest: '<script>if(!(${ test })) cdnizerLoad("${ filepathRel }");</script>',
     files: [
-      //list of host:package pairs for simple packages
-      'cdnjs:angular-scroll',
-      'cdnjs:angular-ui-router',
-      'cdnjs:angular-formly',
-      'cdnjs:angular-bootstrap',
-      'cdnjs:angular-timeago',
-      'cdnjs:angular-elastic',
-      'cdnjs:angulartics',
-      'cdnjs:angular-formly-templates-bootstrap',
-      'cdnjs:api-check@7.5.3',
-      //manual file->cdn settings for packages which could not be
-      //automatically resolved by cdnizer using the above method
+      //manual cdn configuations for all dependencies,
+      //including tests for missing individual angular modules
       {
         file: '**/angular/angular.js',
         cdn: 'google:angular:${ filenameMin }'
@@ -30,46 +21,60 @@ gulp.task('cdnize', ['wiredep'], function () {
       {
         file: '**/angular-sanitize/*.js',
         package: 'angular-sanitize',
-        cdn: 'google:angular:${ filenameMin }'
+        cdn: 'google:angular:${ filenameMin }',
+        test: 'testModule("ngSanitize")'
       },
       {
         file: '**/angular-touch/*.js',
         package: 'angular-touch',
-        cdn: 'google:angular:${ filenameMin }'
+        cdn: 'google:angular:${ filenameMin }',
+        test: 'testModule("ngTouch")'
       },
       {
         file: '**/angular-messages/*.js',
         package: 'angular-messages',
-        cdn: 'google:angular:${ filenameMin }'
+        cdn: 'google:angular:${ filenameMin }',
+        test: 'testModule("ngMessages")'
       },
       {
         file: '**/angular-bootstrap/*.js',
         package: 'angular-bootstrap',
-        cdn: 'cdnjs:angular-ui-bootstrap:${ filenameMin }'
+        cdn: 'cdnjs:angular-ui-bootstrap:${ filenameMin }',
+        test: 'testModule("ui.bootstrap")'
       },
       {
         file: '**/lodash/*.js',
         package: 'lodash',
-        cdn: 'cdnjs:lodash.js:${ filenameMin }'
+        cdn: 'cdnjs:lodash.js:${ filenameMin }',
+        test: 'window._'
       },
       {
-        file: '**/angular-dialog-service/**/*.{js,css}',
+        file: '**/angular-dialog-service/**/*.js',
         package: 'angular-dialog-service',
-        cdn: 'cdnjs:angular-dialog-service:${ filenameMin }'
+        cdn: 'cdnjs:angular-dialog-service:${ filenameMin }',
+        test: 'testModule("dialogs.main")'
+      },
+      {
+        file: '**/angular-dialog-service/**/*.css',
+        package: 'angular-dialog-service',
+        cdn: 'cdnjs:angular-dialog-service:${ filenameMin }',
       },
       {
         file: '**/ui-router-extras/**/*.js',
         package: 'ui-router-extras',
-        cdn: '//cdnjs.cloudflare.com/ajax/libs/ui-router-extras/${ version }/${ filenameMin }'
+        cdn: '//cdnjs.cloudflare.com/ajax/libs/ui-router-extras/${ version }/${ filenameMin }',
+        test: 'testModule("ct.ui.router.extras")'
       },
       {
         file: '**/angulartics-google-analytics/**/*.js',
-        cdn: '//cdnjs.cloudflare.com/ajax/libs/angulartics-google-analytics/0.1.1/${ filenameMin }'
+        cdn: '//cdnjs.cloudflare.com/ajax/libs/angulartics-google-analytics/0.1.1/${ filenameMin }',
+        test: 'testModule("angulartics.google.analytics")'
       },
       {
         file: '**/ngDialog/**/*.js',
         package: 'ngDialog',
-        cdn: 'cdnjs:ng-dialog:js/${ filenameMin }'
+        cdn: 'cdnjs:ng-dialog:js/${ filenameMin }',
+        test: 'testModule("ngDialog")'
       },
       {
         file:'**/ngDialog/**/*.css',
@@ -79,38 +84,86 @@ gulp.task('cdnize', ['wiredep'], function () {
       {
         file: '**/ment.io/**/*.js',
         package: 'ment.io',
-        cdn: '//cdn.jsdelivr.net/angular.ment-io/${ version }/${ filenameMin }'
-      },
-      {
-        file: '**/pdfmake/**/*.js',
-        package: 'pdfmake',
-        cdn: '//cdnjs.cloudflare.com/ajax/libs/pdfmake/${ version }/${ filename }'
+        cdn: '//cdn.jsdelivr.net/angular.ment-io/${ version }/${ filenameMin }',
+        test: 'testModule("mentio")'
       },
       {
         file: '**/angular-resource/*.js',
         package: 'angular-resource',
-        cdn: 'google:angular:${ filenameMin }'
+        cdn: 'google:angular:${ filenameMin }',
+        test: 'testModule("ngResource")'
       },
       {
         file: '**/angular-translate/*.js',
         package: 'angular-translate',
-        cdn: '//cdn.jsdelivr.net/angular.translate/${ version }/${ filenameMin }'
+        cdn: '//cdn.jsdelivr.net/angular.translate/${ version }/${ filenameMin }',
+        test: 'testModule("pascalprecht.translate")'
       },
       {
         file: '**/waypoints/**/*.js',
         package: 'waypoints',
-        cdn: 'cdnjs:waypoints:${ filename }'
+        cdn: 'cdnjs:waypoints:${ filename }',
+        test: 'Waypoint'
       },
       {
         file: '**/angular-ui-grid/**/*.js',
         package: 'angular-ui-grid',
-        cdn: 'cdnjs:angular-ui-grid:${ filenameMin }'
+        cdn: 'cdnjs:angular-ui-grid:${ filenameMin }',
+        test: 'testModule("ui.grid")'
       },
       {
-        file: '**/angular-loading-bar/**/*.{js,css}',
+        file: '**/angular-loading-bar/**/*.js',
         package: 'angular-loading-bar',
-        cdn: 'cdnjs:angular-loading-bar:${ filenameMin }'
+        cdn: 'cdnjs:angular-loading-bar:${ filenameMin }',
+        test: 'testModule("angular-loading-bar")'
       },
+      {
+        file: '**/angular-loading-bar/**/*.css',
+        package: 'angular-loading-bar',
+        cdn: 'cdnjs:angular-loading-bar:${ filenameMin }',
+      },
+      {
+        file: '**/angular-ui-router/**/*.js',
+        package: 'angular-ui-router',
+        cdn: 'cdnjs:angular-ui-router:${ filenameMin }',
+        test: 'testModule("ui.router")'
+      },
+      {
+        file: '**/api-check/**/*.js',
+        package: 'api-check',
+        cdn: 'cdnjs:api-check:${ filenameMin }@7.5.3',
+        test: 'window.apiCheck'
+      },
+      {
+        file: '**/angular-formly/**/*.js',
+        package: 'angular-formly',
+        cdn: 'cdnjs:angular-formly:${ filenameMin }',
+        test: 'testModule("formly")'
+      },
+      {
+        file: '**/angulartics/**/*.js',
+        package: 'angulartics',
+        cdn: 'cdnjs:angulartics:${ filenameMin }',
+        test: 'testModule("angulartics")'
+      },
+      {
+        file: '**/angular-scroll/*.js',
+        package: 'angular-scroll',
+        cdn: 'cdnjs:angular-scroll:${ filenameMin }',
+        test: 'testModule("duScroll")'
+      },
+      {
+        file: '**/angular-elastic/*.js',
+        package: 'angular-elastic',
+        cdn: 'cdnjs:angular-elastic:${ filenameMin }',
+        test: 'testModule("monospaced.elastic")'
+      },
+      {
+        file: '**/angular-formly-templates-bootstrap/**/*.js',
+        package: 'angular-formly-templates-bootstrap',
+        cdn: 'cdnjs:angular-formly-templates-bootstrap:${ filenameMin }',
+        test: 'testModule("formlyBootstrap")'
+      }
     ]
   }))
 
