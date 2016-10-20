e9f62b0f952a7045bb0c47fcb5f165e21f2be8de
Joshua McMichael <jmcmicha@genome.wustl.edu>
2016-10-20 14:46:30 -0500

    added my variant info service

---
+++ src/components/services/MyVariantInfoService.js
@@ -0,0 +1,36 @@
+(function() {
+  'use strict';
+  angular.module('civic.services')
+    .factory('MyVariantInfoResource', MyVariantInfoResource)
+    .factory('MyVariantInfo', MyVariantInfoService);
+
+  // @ngInject
+  function MyVariantInfoResource($resource, $cacheFactory, _) {
+
+    var cache = $cacheFactory('MyVariantInfo'); // default cache doesn't work for some reason
+
+    return $resource('/api/genes/myvariant_info_proxy/:variantId',
+      {
+        variantId: '@variantId'
+      },
+      {
+        get: {
+          isArray: false,
+          cache: cache
+        }
+
+      });
+  }
+
+  // @ngInject
+  function MyVariantInfoService(MyVariantInfoResource) {
+    return {
+      get: function(entrez_id) {
+        return MyVariantInfoResource.get({variantId: entrez_id}).$promise
+          .then(function(response) {
+            return response;
+        });
+      }
+    };
+  }
+})();
