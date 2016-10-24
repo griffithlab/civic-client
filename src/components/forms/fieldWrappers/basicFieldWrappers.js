7522509e00fb6b9cdaadc41967acd6f7b8a847cd
Joshua McMichael <jmcmicha@genome.wustl.edu>
2016-10-24 12:46:34 -0500

    forms now highlight required fields with an asterisk

--- src/components/forms/fieldWrappers/basicFieldWrappers.js
+++ src/components/forms/fieldWrappers/basicFieldWrappers.js
@@ -48,7 +48,7 @@
       name: 'horizontalBootstrapLabel',
       template: [
         '<label for="{{::id}}" class="col-sm-'+ labelColWidth +' control-label">',
-        '{{to.label}}',
+        '{{to.label}}<span ng-if="to.required" style="color:#999;">&nbsp;*</span>',
         '</label>',
         '<div class="col-sm-' + inputColWidth + '">',
         '<formly-transclude></formly-transclude>',
@@ -61,7 +61,7 @@
       name: 'horizontalBootstrapHelp',
       template: [
         '<label for="{{::id}}" class="col-sm-'+ labelColWidth +' control-label">',
-        '{{to.label}}',
+        '{{to.label}}<span ng-if="to.required" style="color:#999;">&nbsp;*</span>',
         '</label>',
         '<div class="col-sm-'+ inputColWidth +'">',
         '<formly-transclude></formly-transclude>',
