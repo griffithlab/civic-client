74186a44c1cf6d7c0ae57539fa16966c00970861
Joshua McMichael <jmcmicha@genome.wustl.edu>
2016-10-27 12:16:49 -0500

    created popup window for tier 2 MyVariant.info data

--- src/app/views/events/variants/summary/myVariantInfo.js
+++ src/app/views/events/variants/summary/myVariantInfo.js
@@ -2,7 +2,7 @@
   'use strict';
   angular.module('civic.events.variants')
     .controller('MyVariantInfoController', MyVariantInfoController)
-//    .controller('MyVariantInfoDialogController', MyVariantInfoDialogController)
+   .controller('MyVariantInfoDialogController', MyVariantInfoDialogController)
     .directive('myVariantInfo', function () {
       return {
         restrict: 'E',
@@ -16,6 +16,33 @@
 
   // @ngInject
   function MyVariantInfoController($scope, ngDialog, _) {
-    console.log('myVariantInfo controller called.');
+    var ctrl = $scope.ctrl = {};
+
+    ctrl.popupOptions = {
+      template: 'app/views/events/variants/summary/myVariantInfoDialog.tpl.html',
+      controller: 'MyVariantInfoDialogController',
+      scope: $scope
+    };
+
+    ctrl.openDialog = function() {
+      if(_.has(ctrl.dialog, 'id')) {
+        ctrl.closeDialog().then(function() {
+          ctrl.dialog = ngDialog.open(ctrl.popupOptions);
+        });
+      } else {
+        ctrl.dialog = ngDialog.open(ctrl.popupOptions);
+      }
+      return ctrl.dialog;
+    };
+
+    ctrl.closeDialog = function() {
+      ctrl.dialog.close();
+      return ctrl.dialog.closePromise.then(function() { _.omit(ctrl, 'dialog'); } );
+    };
+  }
+
+  // @ngInject
+  function MyVariantInfoDialogController($scope) {
+    var ctrl = $scope.$parent.ctrl;
   }
 })();
