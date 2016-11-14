9f311986e9538a8f01994629bc2ec311d2b4d61d
Joshua McMichael <jmcmicha@genome.wustl.edu>
2016-11-14 13:44:04 -0600

    added Agilent to Partners list on About page (closes #567)

--- src/app/pages/AboutCtrl.js
+++ src/app/pages/AboutCtrl.js
@@ -49,6 +49,15 @@
           'x270': 'assets/images/partners/bc_cancer_agency.png'
         },
         'description': 'The BC Cancer Agency\'s Personalized Onco-Genomics (POG) program is a clinical research initiative that is embedding genomic sequencing into real-time treatment planning for BC patients with incurable cancers. Cancer is a complex biological process. We categorize cancers according to their site of origin (e.g. lung, breast, liver, colon) as each one is different, but even within these groupings there are subtypes with differences in response to treatment and overall behaviour. The POG program is a collaborative research study including many BCCA oncologists, pathologists and other clinicians along with the Genome Sciences Centre (GSC), which aims to decode the genome – the entire DNA and RNA inside the cell – of each patient\'s cancer, to understand what is enabling it to grow. Using this genomic data in clinical decision-making should allow us to develop treatment strategies to block its growth, identify clinical trials that the patient may benefit from and potentially identify less toxic and more effective therapeutic options.'
+      },
+      {
+        'id': 0,
+        'name': 'Cartegenia Bench Lab at Agilent Technologies',
+        'avatars': {
+          'x14': 'assets/images/partners/agilent.png',
+          'x270': 'assets/images/partners/agilent.png'
+        },
+        'description': 'Cartagenia Bench Lab is designed to help laboratories involved in clinical genetics and molecular pathology efficiently interpret and report on genomic variants. The clinical-grade software platform – registered as an exempt Class I Medical Device in the U.S. – has become the platform of choice for high-throughput diagnostic labs to help them validate and automate their variant assessment and reporting pipelines. With a range of new features for somatic variant classification, review and curation, the release of Cartagenia Bench Lab 5.0 represents a significant leap forward for molecular pathology labs in the ability to process their data. Bench in general provides direct access to more than 100 curated and publicly available databases with information on actionable variants, trials, drugs and therapy options such as CIViC, COSMIC, N-of-One, OncoMD and CollabRx.'
       }
     ];
   }
