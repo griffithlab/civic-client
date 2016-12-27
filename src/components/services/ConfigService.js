3cdbef4974f0349e0b46af492e10c5ab90a3db8f
Malachi Griffith <malachig@gmail.com>
2016-12-27 21:18:25 +0000

    update descriptions of predisposing and diagnostic in drop down menus

--- src/components/services/ConfigService.js
+++ src/components/services/ConfigService.js
@@ -61,8 +61,9 @@
         },
         evidence_type: {
           'Predictive': 'Evidence pertains to a variant\'s effect on therapeutic response',
-          'Diagnostic': 'Evidence pertains to a variant\'s impact on patient diagnosis',
-          'Prognostic': 'Evidence pertains to a variant\'s impact on disease progression, severity, or patient survival'
+          'Diagnostic': 'Evidence pertains to a variant\'s impact on patient diagnosis (cancer subtype)',
+          'Prognostic': 'Evidence pertains to a variant\'s impact on disease progression, severity, or patient survival',
+          'Predisposing': 'Evidence pertains to a variant\'s role in conferring susceptibility to a disease'
         },
         evidence_level: {
           A: 'Proven/consensus association in human medicine',
