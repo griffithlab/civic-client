2dba158963cb48eb6eb05e348b3ca8468ece1d97
Joshua McMichael <josh@jmcmichael.com>
2017-01-12 14:14:18 -0600

    added new controllers, directives to index.html

--- src/app/index.js
+++ src/app/index.js
@@ -88,19 +88,19 @@
       }
     });
     /*  ui-router debug logging - uncomment these if you run into links/routes silently failing. */
-    // function message(to, toP, from, fromP) {
-    //   return from.name + angular.toJson(fromP) + ' -> ' + to.name + angular.toJson(toP);
-    // }
-    //
-    // $rootScope.$on('$stateChangeStart', function (evt, to, toP, from, fromP) {
-    //   console.log('Start:   ' + message(to, toP, from, fromP));
-    // });
-    // $rootScope.$on('$stateChangeSuccess', function (evt, to, toP, from, fromP) {
-    //   console.log('Success: ' + message(to, toP, from, fromP));
-    // });
-    // $rootScope.$on('$stateChangeError', function (evt, to, toP, from, fromP, err) {
-    //   console.error('Error:   ' + message(to, toP, from, fromP), err);
-    // });
+    function message(to, toP, from, fromP) {
+      return from.name + angular.toJson(fromP) + ' -> ' + to.name + angular.toJson(toP);
+    }
+
+    $rootScope.$on('$stateChangeStart', function (evt, to, toP, from, fromP) {
+      console.log('Start:   ' + message(to, toP, from, fromP));
+    });
+    $rootScope.$on('$stateChangeSuccess', function (evt, to, toP, from, fromP) {
+      console.log('Success: ' + message(to, toP, from, fromP));
+    });
+    $rootScope.$on('$stateChangeError', function (evt, to, toP, from, fromP, err) {
+      console.error('Error:   ' + message(to, toP, from, fromP), err);
+    });
 
   }
 
