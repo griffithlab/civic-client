5a772f812f57726bd4b5eb38530049a4ebf3275b
Aaron Graubert <captianjroot@live.com>
2017-01-26 09:49:38 -0600

    Added additional fields to the multitype handling from myvariant.info
    Also swapped to using the same method for exac data

--- src/app/views/events/variants/summary/myVariantInfo.js
+++ src/app/views/events/variants/summary/myVariantInfo.js
@@ -26,21 +26,21 @@
       'clinvar.hgvs.coding',
       'clinvar.hgvs.genomic',
       'clinvar.hgvs.non-coding',
-      'emv.hgvs'
+      'emv.hgvs',
+      'clinvar.rcv',
+      'exac.ac.ac_adj',
+      'exac.an.an_adj',
+      'snpeff.ann'
     ];
     _.each(multiTypeFields, function(field) {
       var val = _.get(ctrl.variantInfo,field);
-      if (_.isString(val)) { _.set(ctrl.variantInfo, field, [val]); }
+      if (!_.isArray(val)) { _.set(ctrl.variantInfo, field, [val]); }
     });
 
     // calculate adjusted allele frequency
     if(!_.isUndefined(ctrl.variantInfo.exac)) {
       if(!_.isUndefined(ctrl.variantInfo.exac.an) && !_.isUndefined(ctrl.variantInfo.exac.ac)) {
-        //ctrl.variantInfo.exac.adj_allele_freq = _.round(ctrl.variantInfo.exac.ac.ac_adj / ctrl.variantInfo.exac.an.an_adj, 5);
-        ctrl.variantInfo.exac.adj_allele_freq = _.round(
-          _.last(_.flatten(_.values(ctrl.variantInfo.exac.ac))) / _.last(_.flatten(_.values(ctrl.variantInfo.exac.an))),
-          5
-        );
+        ctrl.variantInfo.exac.adj_allele_freq = _.round(ctrl.variantInfo.exac.ac.ac_adj[0] / ctrl.variantInfo.exac.an.an_adj[0], 5);
       }
     }
 
