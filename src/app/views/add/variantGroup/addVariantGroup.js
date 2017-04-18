364e45277636f2a4ecdea15b695960fc176b998a
Joshua McMichael <josh@jmcmichael.com>
2017-04-18 17:09:48 -0500

    updated self-linked anchors to include target attributes

--- src/app/views/add/variantGroup/addVariantGroup.js
+++ src/app/views/add/variantGroup/addVariantGroup.js
@@ -121,7 +121,7 @@
         templateOptions: {
           label: 'Variants',
           entityName: 'Variant',
-          helpText: 'Click the X button to delete a variant, click the + button to add a variant. Note that variants must be known to CIViC to be available for including here. New variants may be added as part of an evidence item using the <a href="/add/evidence/basic">Add Evidence form</a>.',
+          helpText: 'Click the X button to delete a variant, click the + button to add a variant. Note that variants must be known to CIViC to be available for including here. New variants may be added as part of an evidence item using the <a href="/add/evidence/basic" target="_self">Add Evidence form</a>.',
           inputOptions: {
             type: 'typeahead',
             wrapper: null,
