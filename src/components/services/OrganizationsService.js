bae091bc24bd2535dc8f8fab46e5da0d4423d445
Joshua McMichael <josh@jmcmichael.com>
2017-05-09 15:22:19 -0500

    created organizations service

---
+++ src/components/services/OrganizationsService.js
@@ -0,0 +1,67 @@
+(function() {
+  'use strict';
+  angular.module('civic.services')
+    .factory('OrganizationsResource', OrganizationsResource)
+    .factory('Organizations', OrganizationsService);
+
+  // @ngInject
+  function OrganizationsResource($resource, $cacheFactory) {
+    var cache = $cacheFactory.get('$http');
+
+    var cacheInterceptor = function(response) {
+      // console.log(['GenesResource: removing', response.config.url, 'from $http cache.'].join(' '));
+      cache.remove(response.config.url);
+      return response.$promise;
+    };
+
+    return $resource('/api/organizations',
+                     {},
+                     {
+                       query: {
+                         method: 'GET',
+                         isArray: true,
+                         cache: true
+                       },
+                       get: {
+                         method: 'GET',
+                         url: '/api/organizations/:organizationId',
+                         isArray: false,
+                         cache: true
+                       }
+                     });
+  }
+
+  // @ngInject
+  function OrganizationsService($cacheFactory, OrganizationsResource) {
+    var cache = $cacheFactory.get('$http');
+
+    var item = {};
+    var collection = [];
+
+    return {
+      data: {
+        item: item,
+        collection: collection
+      },
+      query: query,
+      get: get
+    };
+
+    function query() {
+      return OrganizationsResource.query().$promise
+        .then(function(response) {
+          angular.copy(response, collection);
+          return response.$promise;
+        });
+    }
+
+    function get(organizationId) {
+      return OrganizationsResource.get({organizationId: organizationId}).$promise
+        .then(function(response) {
+          angular.copy(response, item);
+          return response.$promise;
+        });
+    }
+  }
+
+})();
