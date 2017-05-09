1331ec19fc9f48dc7bd7189a912726c71b0a24da
Joshua McMichael <josh@jmcmichael.com>
2017-05-09 16:43:34 -0500

    added org summary component

---
+++ src/app/views/organizations/components/organizationSummary.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+  angular.module('civic.organizations')
+    .controller('OrganizationSummaryController', OrganizationSummaryController)
+    .directive('organizationSummary', function() {
+      return {
+        restrict: 'E',
+        scope: {
+          organization: '='
+        },
+        controller: 'OrganizationSummaryController',
+        templateUrl: 'app/views/organizations/components/organizationSummary.tpl.html'
+      };
+    });
+
+  // @ngInject
+  function OrganizationSummaryController($scope) {
+    var vm = $scope.vm = {};
+  }
+})();
