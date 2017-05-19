a7f4c8da7fcfd3cd2f52f8bf8b1479c034006e04
Joshua McMichael <josh@jmcmichael.com>
2017-05-19 13:51:19 -0500

    added /downloads proxy redirect for dev server

--- gulp/server.js
+++ gulp/server.js
@@ -24,16 +24,21 @@ function connectInit(baseDir, livereload) {
     middleware: function() {
       return [
         (function() {
-        var options = url.parse('http://127.0.0.1:3000/api');
-        // comment out the line above, and uncomment the line below to query the
-        // production API. Note that login will not work
-        //var options = url.parse('http://civic.genome.wustl.edu/api');
-        options.route = '/api';
-        return proxy(options);
-      })(),
+          var options = url.parse('http://127.0.0.1:3000/api');
+          // comment out the line above, and uncomment the line below to query the
+          // production API. Note that login will not work
+          //var options = url.parse('http://civic.genome.wustl.edu/api');
+          options.route = '/api';
+          return proxy(options);
+        })(),
         (function() {
           var options = url.parse('http://127.0.0.1:3000/badges');
           options.route = '/badges';
+          return proxy(options);
+        })(),
+        (function() {
+          var options = url.parse('http://127.0.0.1:3000/downloads');
+          options.route = '/downloads';
           return proxy(options);
         })(),
         (function() {
