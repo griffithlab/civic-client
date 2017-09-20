4939b8811e60f2a37d7e506f7a4b06cc06984f18
Joshua McMichael <josh@jmcmichael.com>
2017-09-20 16:17:19 -0500

    created AssertionsService

---
+++ src/components/services/AssertionsService.js
@@ -0,0 +1,66 @@
+(function() {
+  'use strict';
+  angular.module('civic.services')
+    .factory('AssertionsResource', AssertionsResource)
+    .factory('Assertions', AssertionsService);
+
+  // @ngInject
+  function AssertionsResource($resource) {
+    //var cache = $cacheFactory.get('$http');
+
+    //var cacheInterceptor = function(response) {
+    //  console.log(['EvidenceResource: removing', response.config.url, 'from $http cache.'].join(' '));
+    //  cache.remove(response.config.url);
+    //  return response.$promise;
+    //};
+
+
+    return $resource('/api/assertions',
+      {},
+      {
+        query: {
+          url: '/api/assertions',
+          method: 'GET',
+          isArray: true,
+          cache: false
+        },
+        get: {
+          url: '/api/assertions/:assertionId',
+          method: 'GET',
+          isArray: false,
+          cache: false
+        }
+      }
+    );
+  }
+
+  // @ngInject
+  function AssertionsService(AssertionsResource) {
+    var collection = { };
+    var item = { };
+
+    return {
+      data: {
+        item: item,
+        collection: collection
+      },
+      query: query,
+      get: get
+    };
+
+    function query() {
+      return AssertionsResource.query().$promise
+        .then(function(response) {
+          angular.copy(response, collection);
+          return response.$promise;
+        });
+    }
+    function get(id) {
+      return AssertionsResource.get({assertionId: id}).$promise
+        .then(function(response) {
+          angular.copy(response, item);
+          return response.$promise;
+        });
+    }
+  }
+})();
