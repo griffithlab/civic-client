5ed549ce9062d27d2f7f6367bbd1cb48ab962b27
Joshua McMichael <josh@jmcmichael.com>
2018-01-04 12:08:56 -0600

    evidence selector field form-group class applied

--- src/components/forms/fieldTypes/evidenceSelectorField.js
+++ src/components/forms/fieldTypes/evidenceSelectorField.js
@@ -16,12 +16,6 @@
         }
       },
       controller: /* @ngInject */ function($scope, _) {
-
-        $scope.selectItem = function(item) {
-          console.log('item selected.');
-          console.log(item);
-        };
-
         $scope.removeItem = function(item) {
           $scope.model.evidence_items = _.reject($scope.model.evidence_items, {id: item.id});
         };
