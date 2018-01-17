53cc6093fa2e1417033ae4cbb91f4f71fd80f1d4
Joshua McMichael <josh@jmcmichael.com>
2018-01-17 17:47:42 -0600

    added flags and comments functions to assertions service

--- src/components/services/AssertionsService.js
+++ src/components/services/AssertionsService.js
@@ -8,71 +8,160 @@
   function AssertionsResource($resource, $cacheFactory) {
     var cache = $cacheFactory.get('$http');
 
-    return $resource('/api/assertions',
-      {},
-      {
-        query: {
-          url: '/api/assertions',
-          method: 'GET',
-          isArray: true,
-          cache: cache
+    return $resource('/api/assertions', {}, {
+      query: {
+        url: '/api/assertions',
+        method: 'GET',
+        isArray: true,
+        cache: cache
+      },
+      get: {
+        url: '/api/assertions/:assertionId',
+        method: 'GET',
+        isArray: false,
+        cache: cache
+      },
+      add: {
+        method: 'POST',
+        cache: false
+      },
+      accept: {
+        url: '/api/assertions/:assertionId/accept',
+        params: {
+          assertionId: '@assertionId'
         },
-        get: {
-          url: '/api/assertions/:assertionId',
-          method: 'GET',
-          isArray: false,
-          cache: cache
+        method: 'POST',
+        cache: false
+      },
+      reject: {
+        url: '/api/assertions/:assertionId/reject',
+        params: {
+          assertionId: '@assertionId'
         },
-        add: {
-          method: 'POST',
-          cache: false
+        method: 'POST',
+        cache: false
+      },
+      queryAcmgCodes: {
+        url: '/api/acmg_codes',
+        method: 'GET',
+        isArray: true,
+        cache: true
+      },
+      apply: {
+        method: 'PATCH',
+        cache: false
+      },
+      queryFlags: {
+        method: 'GET',
+        url: '/api/assertions/:assertionId/flags',
+        isArray: false,
+        cache: cache
+      },
+      submitFlag: {
+        method: 'POST',
+        url: '/api/assertions/:assertionId/flags',
+        cache: false
+      },
+      resolveFlag: {
+        method: 'PATCH',
+        url: '/api/assertions/:assertionId/flags/:flagId',
+        params: {
+          assertionId: '@assertionId',
+          flagId: '@flagId'
         },
-        accept: {
-          url: '/api/assertions/:assertionId/accept',
-          params: {
-            assertionId: '@assertionId'
-          },
-          method: 'POST',
-          cache: false
+        cache: false
+      },
+
+      // Assertion Comments Resources
+      queryComments: {
+        method: 'GET',
+        url: '/api/assertions/:assertionId/comments',
+        isArray: true,
+        cache: cache
+      },
+      getComment: {
+        method: 'GET',
+        url: '/api/assertions/:assertionId/comments/:commentId',
+        params: {
+          assertionId: '@assertionId',
+          commentId: '@commentId'
         },
-        reject: {
-          url: '/api/assertions/:assertionId/reject',
-          params: {
-            assertionId: '@assertionId'
-          },
-          method: 'POST',
-          cache: false
+        isArray: false,
+        cache: cache
+      },
+
+      submitComment: {
+        method: 'POST',
+        url: '/api/assertions/:assertionId/comments',
+        params: {
+          assertionId: '@assertionId'
         },
-        queryAcmgCodes: {
-          url: '/api/acmg_codes',
-          method: 'GET',
-          isArray: true,
-          cache: true
-        }
+        cache: false
+      },
+      updateComment: {
+        method: 'PATCH',
+        url: '/api/assertions/:assertionId/comments/:commentId',
+        params: {
+          assertionId: '@assertionId',
+          commentId: '@commentId'
+        },
+        cache: false
+      },
+      deleteComment: {
+        method: 'DELETE',
+        url: '/api/assertions/:assertionId/comments/:commentId',
+        params: {
+          assertionId: '@assertionId',
+          commentId: '@commentId'
+        },
+        cache: false
       }
-    );
+
+    });
   }
 
   // @ngInject
   function AssertionsService(AssertionsResource, $cacheFactory) {
     var cache = $cacheFactory.get('$http');
 
-    var collection = { };
-    var item = { };
+    // Base Assertion
+    var collection = {};
+    var item = {};
     var acmg_codes = [];
+
+    // Collections
+    var comments = [];
+    var flags = [];
 
     return {
       data: {
         item: item,
         collection: collection,
-        acmg_codes: acmg_codes
+        acmg_codes: acmg_codes,
+        flags: flags,
+        comments: comments
       },
+      // Assertion Base
       query: query,
       get: get,
       add: add,
       accept: accept,
       reject: reject,
-      queryAcmgCodes: queryAcmgCodes
+
+      // Additional Info
+      queryAcmgCodes: queryAcmgCodes,
+
+      // Collections
+      queryFlags: queryFlags,
+      submitFlag: submitFlag,
+      resolveFlag: resolveFlag,
+
+      // Comments
+      queryComments: queryComments,
+      getComment: getComment,
+      submitComment: submitComment,
+      updateComment: updateComment,
+      deleteComment: deleteComment
     };
 
     function query() {
@@ -84,7 +173,9 @@
     }
 
     function get(id) {
-      return AssertionsResource.get({assertionId: id}).$promise
+      return AssertionsResource.get({
+          assertionId: id
+        }).$promise
         .then(function(response) {
           angular.copy(response, item);
           return response.$promise;
@@ -99,7 +190,9 @@
     }
 
     function accept(id) {
-      return AssertionsResource.accept({ assertionId: id}).$promise
+      return AssertionsResource.accept({
+          assertionId: id
+        }).$promise
         .then(function(response) {
           // flush cached assertion and variant
           cache.remove('/api/assertions');
@@ -114,7 +207,9 @@
     }
 
     function reject(id) {
-      return AssertionsResource.reject({ assertionId: id }).$promise
+      return AssertionsResource.reject({
+          assertionId: id
+        }).$promise
         .then(function(response) {
           // flush cached assertion and variant
           cache.remove('/api/assertions');
@@ -132,6 +227,99 @@
       return AssertionsResource.queryAcmgCodes().$promise
         .then(function(response) {
           angular.copy(response, acmg_codes);
+          return response.$promise;
+        });
+    }
+
+    function queryFlags(assertionId) {
+      return AssertionsResource.queryFlags({assertionId: assertionId}).$promise
+        .then(function(response) {
+          angular.copy(response.records, flags);
+          return response.$promise;
+        });
+    }
+
+    function submitFlag(reqObj) {
+      reqObj.assertionId = reqObj.entityId;
+      return AssertionsResource.submitFlag(reqObj).$promise
+        .then(function(response) {
+          cache.remove('/api/assertions/' + reqObj.assertionId + '/flags');
+          queryFlags(reqObj.assertionId);
+
+          // flush subscriptions and refresh
+          cache.remove('/api/subscriptions?count=999');
+          Subscriptions.query();
+
+          return response.$promise;
+        });
+    }
+    function resolveFlag(reqObj) {
+      reqObj.assertionId = reqObj.entityId;
+      reqObj.state = 'resolved';
+      return AssertionsResource.resolveFlag(reqObj).$promise
+        .then(function(response) {
+          cache.remove('/api/assertions/' + reqObj.assertionId + '/flags');
+          queryFlags(reqObj.assertionId);
+
+          // flush subscriptions and refresh
+          cache.remove('/api/subscriptions?count=999');
+          Subscriptions.query();
+
+          return response.$promise;
+        });
+    }
+
+    function queryComments(assertionId) {
+      return AssertionsResource.queryComments({
+        assertionId: assertionId
+      }).$promise
+        .then(function(response) {
+          angular.copy(response, comments);
+          return response.$promise;
+        });
+    }
+
+    function getComment(assertionId, commentId) {
+      return AssertionsResource.getComment({
+        assertionId: assertionId,
+        commentId: commentId
+      }).$promise
+        .then(function(response) {
+          return response.$promise;
+        });
+    }
+
+    function submitComment(reqObj) {
+      return AssertionsResource.submitComment(reqObj).$promise
+        .then(function(response) {
+          cache.remove('/api/assertions/' + reqObj.assertionId + '/comments');
+          queryComments(reqObj.assertionId);
+
+          // flush subscriptions and refresh
+          cache.remove('/api/subscriptions?count=999');
+          Subscriptions.query();
+
+          return response.$promise;
+        });
+    }
+
+    function updateComment(reqObj) {
+      return AssertionsResource.updateComment(reqObj).$promise
+        .then(function(response) {
+          cache.remove('/api/assertions/' + reqObj.assertionId + '/comments');
+          queryComments(reqObj.assertionId);
+          return response.$promise;
+        });
+    }
+
+    function deleteComment(commentId) {
+      return AssertionsResource.deleteComment({
+        assertionId: item.id,
+        commentId: commentId
+      }).$promise
+        .then(function(response) {
+          cache.remove('/api/assertions/' + item.id + '/comments');
+          queryComments(item.id);
           return response.$promise;
         });
     }
