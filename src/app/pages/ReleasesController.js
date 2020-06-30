b631b9407c77585ad769fad153c9397350fdccf4
Joshua McMichael <josh@jmcmichael.com>
2020-06-30 14:04:40 -0500

    releases controller now munges response into schema better suited for datagrid

--- src/app/pages/ReleasesController.js
+++ src/app/pages/ReleasesController.js
@@ -9,11 +9,27 @@
     var vm = this;
     // TODO move releases init to ui-router state resolve
     Releases.initBase().then(function(releases){
+      var releaseTemplate = {
+        GeneSummaries: null,
+        VariantSummaries: null,
+        ClinicalEvidenceSummaries: null,
+        VariantGroupSummaries: null,
+        AssertionSummaries: null,
+        civic_accepted: null,
+        civic_accepted_and_submitted: null
+      };
       vm.releases = _.map(releases[0], function(release) {
-        release.fileUrls = _.map(release.files, function(filename) {
-          return filename;
+        // assign note, then for each key in releaseTemplate, attach URL from file list, if found
+        var releaseObj = {
+          note: release.note
+        };
+        _.forEach(releaseTemplate, function(value, fileType) {
+          releaseObj[fileType] = _.find(release.files, function(val) {
+            return val.includes(fileType);
+          });
         });
-        return release;
+        console.log(releaseObj);
+        return releaseObj;
       });
 
     });
