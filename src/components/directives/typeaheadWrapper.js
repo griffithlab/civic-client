28a5d98301518cd5b0db119705201b3716df05df
Joshua McMichael <josh@jmcmichael.com>
2020-07-20 16:31:26 -0500

    added some helpful comments to typeaheadWrapper component

--- src/components/directives/typeaheadWrapper.js
+++ src/components/directives/typeaheadWrapper.js
@@ -12,6 +12,7 @@
       link: function(scope, elem, attrs)
       {
         var template = '<input ';
+        // basic HTML & angular attributes
         template+='type="'+attrs['type']+'" ';
         template+='placeholder="{{ to.placeholder }}"';
         if(attrs['formlyCustomValidation'] !== undefined) { template += 'formlyCustomValidation '; }
@@ -26,6 +27,7 @@
         template+='ng-model="'+attrs['ngModel']+'" ';
         template+='ng-model-options="'+attrs['ngModelOptions']+'" ';
         template+='class="'+attrs['class']+'" ';
+        // existing ng-bootstrap attributes (we just copy these from the element)
         template+='typeahead-editable="'+attrs['typeaheadEditable']+'" ';
         template+='typeahead-focus-first="'+attrs['typeaheadFocusFirst']+'" ';
         template+='typeahead-append-to-body="'+attrs['typeaheadAppendToBody']+'" ';
@@ -38,6 +40,7 @@
         var templateUrl = _.get(scope,attrs['typeaheadTemplateUrl']);
         if(templateUrl !== undefined) { template+='typeahead-template-url="'+templateUrl+'" '; }
 
+        // additional elements
         var popupTemplateUrl = _.get(scope,'to.popupTemplateUrl');
         if(popupTemplateUrl !== undefined) { template+='typeahead-popup-template-url="'+popupTemplateUrl+'" '; }
 
